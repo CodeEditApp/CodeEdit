0a9eba024f6e8432a2a5d824292a840b252d3bbf
Austin Condiff <austin.condiff@gmail.com>
2022-03-12 15:50:15 -0600

    Revert "Project's folder opening, basic Settings and open by URL (#2)"
    This reverts commit 599302fe72ff7b9cf1a20361ffde16440deef85f.

--- CodeEdit/CodeEditApp.swift
+++ CodeEdit/CodeEditApp.swift
@@ -9,20 +9,11 @@ import SwiftUI
 
 @main
 struct CodeEditApp: App {
-    @NSApplicationDelegateAdaptor private var appDelegate: CodeEditorAppDelegate
-    
     var body: some Scene {
         WindowGroup {
             ContentView()
-                .environmentObject(appDelegate)
         }.commands {
             SidebarCommands()
-        }
-            .windowStyle(.hiddenTitleBar)
-            .windowToolbarStyle(.unified)
-            .handlesExternalEvents(matching: ["open"])
-        Settings {
-            SettingsView()
         }
     }
 }
--- CodeEdit/ContentView.swift
+++ CodeEdit/ContentView.swift
@@ -20,9 +20,6 @@ struct MainContentView: View {
 }
 
 struct ContentView: View {
-    @EnvironmentObject var appDelegate: CodeEditorAppDelegate
-    @SceneStorage("ContentView.path") private var path: String = ""
-    
     @State private var queryString = ""
     
     private let items = ["One", "Two", "Three", "Four", "Five"]
@@ -30,84 +27,58 @@ struct ContentView: View {
 
     var body: some View {
         NavigationView {
-            sidebar
+            VStack {
+                List(selection: $selection) {
+                    NavigationLink(
+                        destination: MainContentView()
+                            .navigationTitle("Home")
+                            .navigationSubtitle("Dashboard"),
+                        label: {
+                            Image(systemName: "house")
+                                .foregroundColor(.secondary)
+                            Text("Home")
+                        }
+                    ).id("home")
+                    Section(header: Text("Items")) {
+                        ForEach(items, id: \.self) { item in
+                            NavigationLink(
+                                destination: Text("Item \(item)")
+                                    .navigationTitle("Item \(item)"),
+                                label: {
+                                    Image(systemName: "folder")
+                                        .foregroundColor(.secondary)
+                                    Text("Item \(item)")
+                                }
+                            )
+                        }
+                    }
+                }
+                .listStyle(SidebarListStyle())
+            }
+
+            .toolbar {
+            ToolbarItem(placement: .automatic) {
+                    Button(action: toggleSidebar, label: {
+                        Image(systemName: "sidebar.leading")
+                    }).help("Show/Hide Sidebar")
+                }
+                ToolbarItem(placement: .navigation) {
+                    Button(action: toggleSidebar, label: {
+                        Image(systemName: "chevron.left")
+                    }).help("Back")
+                }
+                ToolbarItem(placement: .navigation) {
+                    Button(action: toggleSidebar, label: {
+                        Image(systemName: "chevron.right")
+                    }).disabled(true).help("Fordward")
+                }
+
+            }
             
-            Text("Location: \(path)")
         }
-        .toolbar {
-            ToolbarItem(placement: .navigation) {
-                Button(action: {}) {
-                    Image(systemName: "chevron.left")
-                }
-                .help("Back")
-            }
-            ToolbarItem(placement: .navigation) {
-                Button(action: {}){
-                    Image(systemName: "chevron.right")
-                }
-                .disabled(true)
-                .help("Forward")
-            }
-        }
-        .frame(minWidth: 800, minHeight: 600)
-        .onOpenURL { url in
-            path = url.path
-        }
-        .onAppear {
-            DispatchQueue.main.asyncAfter(deadline: .now() + 0.01) {
-                if path == "" {
-                    if let url = self.appDelegate.newProjectURL() {
-                        self.path = url.path
-                        
-                        // TODO: additional project initialization
-                    } else {
-                        NSApplication.shared.keyWindow?.close()
-                    }
-                }
-            }
-        }
+        
+        
     }
-    
-    var sidebar: some View {
-        VStack {
-            List(selection: $selection) {
-                NavigationLink(
-                    destination: MainContentView()
-                        .navigationTitle("Home")
-                        .navigationSubtitle("Dashboard"),
-                    label: {
-                        Image(systemName: "house")
-                            .foregroundColor(.secondary)
-                        Text("Home")
-                    }
-                ).id("home")
-                Section(header: Text("Items")) {
-                    ForEach(items, id: \.self) { item in
-                        NavigationLink(
-                            destination: Text("Item \(item)")
-                                .navigationTitle("Item \(item)"),
-                            label: {
-                                Image(systemName: "folder")
-                                    .foregroundColor(.secondary)
-                                Text("Item \(item)")
-                            }
-                        )
-                    }
-                }
-            }
-            .listStyle(SidebarListStyle())
-        }
-        .frame(minWidth: 250)
-        .toolbar {
-            ToolbarItem(placement: .primaryAction) {
-                Button(action: toggleSidebar) {
-                    Image(systemName: "sidebar.leading")
-                }
-                .help("Show/Hide Sidebar")
-            }
-        }
-    }
-    
     private func toggleSidebar() {
         #if os(iOS)
         #else
