12b1e1e90a01069326c3dd7cec1a69f9583daa7d
Luke <l.pistrol@gmail.com>
2022-03-16 17:51:28 +0100

    Added file images defending on file extension

--- CodeEdit/Models/FileItem.swift
+++ CodeEdit/Models/FileItem.swift
@@ -14,9 +14,39 @@ struct FileItem: Hashable, Identifiable {
     var systemImage: String {
         switch children {
         case nil:
-            return "doc.plaintext"
+            return fileImage
         case .some(let children):
             return children.isEmpty ? "folder" : "folder.fill"
         }
     }
+
+	var fileImage: String {
+		switch fileType {
+		case "json":
+			return "curlybraces"
+		case "css":
+			return "number"
+		case "js":
+			return "atom"
+		case "swift":
+			return "swift"
+		case "env":
+			return "gearshape.fill"
+		case "gitignore":
+			return "arrow.triangle.branch"
+		case "png", "jpg", "jpeg", "ico":
+			return "photo"
+		case "svg":
+			return "square.fill.on.circle.fill"
+		default:
+			return "doc.plaintext"
+		}
+	}
+
+	private var fileType: String {
+		if let comp = url.lastPathComponent.components(separatedBy: ".").last {
+			return comp
+		}
+		return ""
+	}
 }
