e17ca15fd0a7df70ffb22a3a6b2d22bb0d4e3ff0
Luke <l.pistrol@gmail.com>
2022-03-16 18:08:33 +0100

    Improvement: file icons depending on file extension (#15)
    * Added file images defending on file extension
    
    * Added file icons depending on file extension
    
    * changed default icon to "doc"
    
    * *.js files use "curlybraces" icon, *.jsx files use "atom" icon

--- CodeEdit/Models/FileItem.swift
+++ CodeEdit/Models/FileItem.swift
@@ -14,9 +14,39 @@ struct FileItem: Hashable, Identifiable {
     var systemImage: String {
         switch children {
         case nil:
-            return "doc.plaintext"
+            return fileIcon
         case .some(let children):
             return children.isEmpty ? "folder" : "folder.fill"
         }
     }
+
+	var fileIcon: String {
+		switch fileType {
+		case "json", "js":
+			return "curlybraces"
+		case "css":
+			return "number"
+		case "jsx":
+			return "atom"
+		case "swift":
+			return "swift"
+		case "env":
+			return "gearshape.fill"
+		case "gitignore":
+			return "arrow.triangle.branch"
+		case "png", "jpg", "jpeg", "ico":
+			return "photo"
+		case "svg":
+			return "square.fill.on.circle.fill"
+		default:
+			return "doc"
+		}
+	}
+
+	private var fileType: String {
+		if let comp = url.lastPathComponent.components(separatedBy: ".").last {
+			return comp
+		}
+		return ""
+	}
 }
