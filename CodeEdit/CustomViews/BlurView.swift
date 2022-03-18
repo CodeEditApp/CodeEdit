12d773b007b57aee38150c38eb474938f18e760f
Wesley de Groot <wes@vista.aero>
2022-03-18 22:06:45 +0100

    Lint: BlurView.swift

--- CodeEdit/CustomViews/BlurView.swift
+++ CodeEdit/CustomViews/BlurView.swift
@@ -9,17 +9,15 @@ import Foundation
 import SwiftUI
 
 struct BlurView: NSViewRepresentable {
-    
     var material: NSVisualEffectView.Material
     var blendingMode: NSVisualEffectView.BlendingMode
-    
+
     func makeNSView(context: Context) -> NSVisualEffectView {
         let view = NSVisualEffectView()
         view.material = material
         view.blendingMode = blendingMode
         return view
     }
-    
+
     func updateNSView(_ nsView: NSVisualEffectView, context: Context) { }
-    
 }
