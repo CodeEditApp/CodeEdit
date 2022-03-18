18ff2cef4d5acd19cbce023158450f673021566d
Wesley de Groot <wes@vista.aero>
2022-03-18 21:57:34 +0100

    Lint: Appearance.swift

--- CodeEdit/Settings/Models/Appearances.swift
+++ CodeEdit/Settings/Models/Appearances.swift
@@ -11,21 +11,20 @@ enum Appearances: String, CaseIterable, Hashable {
     case system
     case light
     case dark
-    
+
     func applyAppearance() {
         switch self {
         case .system:
             NSApp.appearance = nil
-            break
+
         case .dark:
             NSApp.appearance = .init(named: .darkAqua)
-            break
+
         case .light:
             NSApp.appearance = .init(named: .aqua)
-            break
         }
     }
-    
+
     static let `default` = Appearances.system
     static let storageKey = "appearance"
 }
