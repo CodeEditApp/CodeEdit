26ff8ddd29facbde2854a516bd86e3587e34c0d5
Wesley de Groot <wes@vista.aero>
2022-03-18 14:39:26 +0100

    Added support for ⌘1-9

--- CodeEdit/TabBar/TabBar.swift
+++ CodeEdit/TabBar/TabBar.swift
@@ -12,7 +12,7 @@ struct TabBar: View {
     var windowController: NSWindowController
     @ObservedObject var workspace: WorkspaceDocument
 
-	var tabBarHeight = 28.0
+    var tabBarHeight = 28.0
 
     var body: some View {
         VStack(spacing: 0.0) {
@@ -33,6 +33,10 @@ struct TabBar: View {
                             .animation(.easeOut(duration: 0.2), value: workspace.openFileItems)
                             .buttonStyle(.plain)
                             .id(item.id)
+                            .keyboardShortcut(
+                                self.getTabId(fileName: item.fileName),
+                                modifiers: [.command]
+                            )
                         }
                     }
                     .onChange(of: workspace.selectedId) { newValue in
@@ -50,5 +54,46 @@ struct TabBar: View {
         .background(Material.regular)
     }
 
-	
+    func getTabId(fileName: String) -> KeyEquivalent {
+        var tabID = 0
+
+        for i in 0..<10 {
+            if workspace.openFileItems.count > i,
+               workspace.openFileItems[i].fileName == fileName {
+                tabID = i + 1
+            }
+        }
+
+        switch tabID {
+        case 1:
+            return "1"
+
+        case 2:
+            return "2"
+
+        case 3:
+            return "3"
+
+        case 4:
+            return "4"
+
+        case 5:
+            return "5"
+
+        case 6:
+            return "6"
+
+        case 7:
+            return "7"
+
+        case 8:
+            return "8"
+
+        case 9:
+            return "9"
+
+        default:
+            return "0"
+        }
+    }
 }
