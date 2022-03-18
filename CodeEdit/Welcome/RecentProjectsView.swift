256b2056f0f3b03621607d30af1fc7fd8df1554e
Ziyuan Zhao <ninjiacoder@gmail.com>
2022-03-18 23:02:37 +0800

    Remove useless extension

--- CodeEdit/Welcome/RecentProjectsView.swift
+++ CodeEdit/Welcome/RecentProjectsView.swift
@@ -7,26 +7,6 @@
 
 import SwiftUI
 
-extension Array: RawRepresentable where Element: Codable {
-    public init?(rawValue: String) {
-        guard let data = rawValue.data(using: .utf8),
-              let result = try? JSONDecoder().decode([Element].self, from: data)
-        else {
-            return nil
-        }
-        self = result
-    }
-    
-    public var rawValue: String {
-        guard let data = try? JSONEncoder().encode(self),
-              let result = String(data: data, encoding: .utf8)
-        else {
-            return "[]"
-        }
-        return result
-    }
-}
-
 struct RecentProjectsView: View {
     @State var recentProjectPaths: [String] = UserDefaults.standard.array(forKey: "recentProjectPaths") as? [String] ?? []
     @State var selectedProjectPath = ""
