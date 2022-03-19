23a40841fe488dab7bc86e44fd5b262d38a13f28
Austin Condiff <austin.condiff@gmail.com>
2022-03-19 05:09:58 -0500

    Fixed typography, added hover state to close button, added transition on hover of welcome screen, added vibrancy to recent files list (#81)

--- CodeEdit/CustomViews/BlurView.swift
+++ CodeEdit/CustomViews/BlurView.swift
@@ -9,17 +9,21 @@ import Foundation
 import SwiftUI
 
 struct BlurView: NSViewRepresentable {
+    let material: NSVisualEffectView.Material
+    let blendingMode: NSVisualEffectView.BlendingMode
     
-    var material: NSVisualEffectView.Material
-    var blendingMode: NSVisualEffectView.BlendingMode
-    
-    func makeNSView(context: Context) -> NSVisualEffectView {
-        let view = NSVisualEffectView()
-        view.material = material
-        view.blendingMode = blendingMode
-        return view
+    func makeNSView(context: Context) -> NSVisualEffectView
+    {
+        let visualEffectView = NSVisualEffectView()
+        visualEffectView.material = material
+        visualEffectView.blendingMode = blendingMode
+        visualEffectView.state = NSVisualEffectView.State.active
+        return visualEffectView
     }
-    
-    func updateNSView(_ nsView: NSVisualEffectView, context: Context) { }
-    
+
+    func updateNSView(_ visualEffectView: NSVisualEffectView, context: Context)
+    {
+        visualEffectView.material = material
+        visualEffectView.blendingMode = blendingMode
+    }
 }
