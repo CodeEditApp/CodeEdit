68f1716eb4de641781864f4479bafd71577450ab
Pavel Kasila <pavel.kasila@gmail.com>
2022-03-19 15:23:38 +0300

    Update script

--- .github/scripts/remove_todos.sh
+++ .github/scripts/remove_todos.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-TAGS="TODO:|FIXME:"
-echo "searching ${SRCROOT} for ${TAGS}"
-find "${SRCROOT}" \( -name "*.swift" \) -print0 | xargs -0 egrep --with-filename --line-number --only-matching "($TAGS).*\$" | perl -p -e "s/($TAGS)/ warning: \$1/"
+grep -rl TODO . | xargs sed -i 's/TODO/notaTODO/g'
+grep -rl FIXME . | xargs sed -i 's/FIXME/notaFIXME/g'
