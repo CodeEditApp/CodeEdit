a9c5cc98f2868894cffd1a35e20e998cd3ef3da7
Pavel Kasila <pavel.kasila@gmail.com>
2022-03-19 15:20:13 +0300

    Add remove TODOs to GitHub Actions

---
+++ .github/scripts/remove_todos.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+TAGS="TODO:|FIXME:"
+echo "searching ${SRCROOT} for ${TAGS}"
+find "${SRCROOT}" \( -name "*.swift" \) -print0 | xargs -0 egrep --with-filename --line-number --only-matching "($TAGS).*\$" | perl -p -e "s/($TAGS)/ warning: \$1/"
