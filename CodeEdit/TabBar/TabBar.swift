1e77bb4900fbcee305d6204461123f62a937dc49
viktorstrate <viktorstrate@gmail.com>
2022-03-20 19:47:19 +0100

    Refactor TabBarItems

--- CodeEdit/TabBar/TabBar.swift
+++ CodeEdit/TabBar/TabBar.swift
@@ -31,38 +31,10 @@ struct TabBar: View {
                 ScrollViewReader { value in
                     HStack(alignment: .center, spacing: 0.0) {
                         ForEach(workspace.openFileItems, id: \.id) { item in
-                            let isActive = workspace.selectedId == item.id
-
-                            Button(
-                                action: { workspace.selectedId = item.id },
-                                label: {
-                                    if isActive {
-                                        TabBarItem(
-                                            item: item,
-                                            windowController: windowController,
-                                            workspace: workspace
-                                        )
-                                        .background(
-                                            BlurView(
-                                                material: NSVisualEffectView.Material.titlebar,
-                                                blendingMode: NSVisualEffectView.BlendingMode.withinWindow
-                                            )
-                                        )
-                                    } else {
-                                        TabBarItem(
-                                            item: item,
-                                            windowController: windowController,
-                                            workspace: workspace
-                                        )
-                                    }
-                                }
-                            )
-                            .animation(.easeOut(duration: 0.2), value: workspace.openFileItems)
-                            .buttonStyle(.plain)
-                            .id(item.id)
-                            .keyboardShortcut(
-                                self.getTabId(fileName: item.fileName),
-                                modifiers: [.command]
+                            TabBarItem(
+                                item: item,
+                                windowController: windowController,
+                                workspace: workspace
                             )
                         }
                     }
@@ -74,16 +46,5 @@ struct TabBar: View {
         }
         .background(BlurView(material: NSVisualEffectView.Material.windowBackground,
                              blendingMode: NSVisualEffectView.BlendingMode.withinWindow))
-    }
-
-    func getTabId(fileName: String) -> KeyEquivalent {
-        for counter in 0..<9 where workspace.openFileItems.count > counter &&
-        workspace.openFileItems[counter].fileName == fileName {
-            return KeyEquivalent.init(
-                Character.init("\(counter + 1)")
-            )
-        }
-
-        return "0"
     }
 }
--- CodeEdit/TabBar/TabBarItem.swift
+++ CodeEdit/TabBar/TabBarItem.swift
@@ -14,8 +14,13 @@ struct TabBarItem: View {
     var windowController: NSWindowController
     @ObservedObject var workspace: WorkspaceDocument
     var tabBarHeight: Double = 28.0
-    var body: some View {
-        let isActive = item.id == workspace.selectedId
+    
+    var isActive: Bool {
+        item.id == workspace.selectedId
+    }
+    
+    @ViewBuilder
+    var content: some View {
         HStack(spacing: 0.0) {
             FileTabRow(fileItem: item, isSelected: isActive, isHovering: isHovering) {
                 withAnimation {
@@ -41,5 +46,48 @@ struct TabBarItem: View {
                 }
             }
         }
+    }
+    
+    var body: some View {
+        Button(action: { workspace.selectedId = item.id }) {
+            content
+                .background(
+                    isActive ? AnyView(BlurView(
+                        material: NSVisualEffectView.Material.titlebar,
+                        blendingMode: NSVisualEffectView.BlendingMode.withinWindow
+                    )) : AnyView(EmptyView())
+                )
+        }
+        .animation(.easeOut(duration: 0.2), value: workspace.openFileItems)
+        .buttonStyle(.plain)
+        .id(item.id)
+        .keyboardShortcut(
+            workspace.getTabId(fileName: item.fileName),
+            modifiers: [.command]
+        )
+        .contextMenu {
+            Button("Close Tab") {
+                
+            }
+            Button("Close Other Tab") {
+                
+            }
+            Button("Close Tabs to the Right") {
+                
+            }
+        }
+    }
+}
+
+extension WorkspaceDocument {
+    func getTabId(fileName: String) -> KeyEquivalent {
+        for counter in 0..<9 where self.openFileItems.count > counter &&
+        self.openFileItems[counter].fileName == fileName {
+            return KeyEquivalent.init(
+                Character.init("\(counter + 1)")
+            )
+        }
+
+        return "0"
     }
 }
