35351b28cc142d16e9a2e7fce7063aee189ad892
Luke <lukas@pistrol.com>
2022-03-25 01:11:16 +0100

    fixed colors and spacing

--- CodeEdit/NavigatorSidebar/NavigatorSidebarItem.swift
+++ CodeEdit/NavigatorSidebar/NavigatorSidebarItem.swift
@@ -11,6 +11,8 @@ import WorkspaceClient
 import CodeFile
 
 struct NavigatorSidebarItem: View {
+	@Environment(\.controlActiveState) var activeState
+	@Environment(\.colorScheme) var colorScheme
 	@AppStorage(FileIconStyle.storageKey) var iconStyle: FileIconStyle = .default
 	var item: WorkspaceClient.FileItem
 	@ObservedObject var workspace: WorkspaceDocument
@@ -34,15 +36,15 @@ struct NavigatorSidebarItem: View {
 		} label: {
 			HStack {
 				Image(systemName: item.systemImage)
+					.resizable()
+					.aspectRatio(contentMode: .fit)
+					.frame(width: 12, height: 12, alignment: .center)
 					.foregroundColor(
-						workspace.selectionState.selectedId == item.id ?
-							.white :
-							iconStyle == .color ? item.iconColor : .secondary
+						selectionIconColor
 					)
-					.font(.callout.bold())
 				Text(item.url.lastPathComponent)
-					.foregroundColor(workspace.selectionState.selectedId == item.id ? .white : .primary)
-					.font(.callout)
+					.foregroundColor(selectionForegroundColor)
+					.font(.subheadline)
 					.frame(maxWidth: .infinity, alignment: .leading)
 					.contentShape(Rectangle())
 			}
@@ -50,11 +52,13 @@ struct NavigatorSidebarItem: View {
 			.padding(.leading, indentLevel * 16 + 12)
 		}
 		.buttonStyle(.plain)
-		.frame(height: 22, alignment: .center)
+		.frame(height: 20, alignment: .center)
 		.padding(.horizontal, 4)
 		.background {
-			RoundedRectangle(cornerRadius: 4)
-				.foregroundColor(workspace.selectionState.selectedId == item.id ? Color.blue : .clear)
+			RoundedRectangle(cornerRadius: 5)
+				.foregroundColor(
+					selectionBackgroundColor
+				)
 		}
 		.contextMenu { contextMenuContent(false) }
 	}
@@ -67,24 +71,32 @@ struct NavigatorSidebarItem: View {
 					.font(.callout.bold())
 					.rotationEffect(.degrees(isExpanded ? 90 : 0))
 					.padding(4)
+					.foregroundColor(.secondary)
 					.contentShape(Rectangle())
 					.onTapGesture {
 						withAnimation(.default.speed(1.2)) {
 							isExpanded.toggle()
 						}
 					}
-				Label(item.url.lastPathComponent, systemImage: item.systemImage)
-					.foregroundColor(.secondary)
-					.font(.callout)
-					.frame(maxWidth: .infinity, alignment: .leading)
-					.contentShape(Rectangle())
-					.onTapGesture(count: 2) {
-						withAnimation(.default.speed(1.2)) {
-							isExpanded.toggle()
+				HStack {
+					Image(systemName: item.systemImage)
+						.resizable()
+						.aspectRatio(contentMode: .fit)
+						.frame(width: 12, height: 12, alignment: .center)
+						.foregroundColor(folderColor)
+					Text(item.url.lastPathComponent)
+						.foregroundColor(.primary)
+						.font(.callout)
+						.frame(maxWidth: .infinity, alignment: .leading)
+						.contentShape(Rectangle())
+						.onTapGesture(count: 2) {
+							withAnimation(.default.speed(1.2)) {
+								isExpanded.toggle()
+							}
 						}
-					}
+				}
 			}
-			.frame(height: 22, alignment: .center)
+			.frame(height: 20, alignment: .center)
 			.padding(.leading, indentLevel * 16)
 			.contextMenu { contextMenuContent(true) }
 			ZStack {
@@ -105,6 +117,38 @@ struct NavigatorSidebarItem: View {
 			}
 			.clipped()
 		}
+	}
+
+	private var selectionForegroundColor: Color {
+		if workspace.selectionState.selectedId != item.id { return .primary }
+		if activeState == .inactive { return .primary }
+		return .white
+	}
+
+	private var selectionIconColor: Color {
+		if workspace.selectionState.selectedId == item.id {
+			return activeState == .inactive ? item.iconColor : .white
+		}
+		if iconStyle == .color { return item.iconColor }
+		return .secondary
+	}
+
+	private var selectionBackgroundColor: Color {
+		if workspace.selectionState.selectedId != item.id { return .clear }
+		if activeState == .inactive { return unfocusedColor }
+		return focusedColor
+	}
+
+	private var focusedColor: Color {
+		.accentColor.opacity(0.75)
+	}
+
+	private var unfocusedColor: Color {
+		colorScheme == .light ? .black.opacity(0.11) : .white.opacity(0.12)
+	}
+
+	private var folderColor: Color {
+		Color(nsColor: .secondaryLabelColor).opacity(0.75)
 	}
 
 	// TODO: Some implementations still need to be done
