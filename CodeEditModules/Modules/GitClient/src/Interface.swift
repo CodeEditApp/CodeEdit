9a55c23f44698f7eeda9f218e598ee07d14c5bf9
marcocarnevali <marcocarnevali@me.com>
2022-03-28 13:38:18 +0200

    [GitClient] feat: added option to get file commit history

--- CodeEditModules/Modules/GitClient/src/Interface.swift
+++ CodeEditModules/Modules/GitClient/src/Interface.swift
@@ -12,14 +12,19 @@ public struct GitClient {
     public var getBranches: () throws -> [String]
     public var checkoutBranch: (String) throws -> Void
     public var pull: () throws -> Void
-    public var getCommitHistory: (_ entries: Int?) throws -> [Commit]
+    /// Get commit history
+    /// - Parameters:
+    ///   - entries: number of commits we want to fetch. Will use max if nil
+    ///   - fileLocalPath: specify a local file (e.g. `CodeEditModules/Package.swift`)
+    ///   to retrieve a file commit history. Optional.
+    public var getCommitHistory: (_ entries: Int?, _ fileLocalPath: String?) throws -> [Commit]
 
     init(
         getCurrentBranchName: @escaping () throws -> String,
         getBranches: @escaping () throws -> [String],
         checkoutBranch: @escaping (String) throws -> Void,
         pull: @escaping () throws -> Void,
-        getCommitHistory: @escaping (_ entries: Int?) throws -> [Commit]
+        getCommitHistory: @escaping (_ entries: Int?, _ fileLocalPath: String?) throws -> [Commit]
     ) {
         self.getCurrentBranchName = getCurrentBranchName
         self.getBranches = getBranches
