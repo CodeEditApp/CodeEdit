18ff4a2b275bcb414c2f0fe88d28cb495eb1ae63
Tihan-Nico <tihan-nico@plexus.dev>
2022-03-30 15:47:06 +0200

    Added a custom context menu for the code editor

--- CodeEditModules/Modules/CodeFile/src/TextView/CodeEditorTextView.swift
+++ CodeEditModules/Modules/CodeFile/src/TextView/CodeEditorTextView.swift
@@ -89,4 +89,153 @@ public class CodeEditorTextView: NSTextView {
             replacementRange: selectedRange()
         )
     }
+
+    /// input a backslash (\\)
+    @IBAction func inputBackSlash(_ sender: Any?) {
+        super.insertText(
+            "\\",
+            replacementRange: selectedRange()
+        )
+    }
+
+    /// Override of the default context menu in the editor...
+    public override func menu(for event: NSEvent) -> NSMenu? {
+        guard var menu = super.menu(for: event) else { return nil }
+
+        menu = helpMenu(menu)
+        menu = codeMenu(menu)
+        menu = gitMenu(menu)
+        menu = removeSubMenus(menu)
+        menu = removeTextMenu(menu)
+
+        return menu
+    }
+
+    func helpMenu(_ menu: NSMenu) -> NSMenu {
+        menu.insertItem(withTitle: "Jump To Definition",
+                        action: nil,
+                        keyEquivalent: "",
+                        at: 1)
+
+        menu.insertItem(withTitle: "Show Code Actions",
+                        action: nil,
+                        keyEquivalent: "",
+                        at: 2)
+
+        menu.insertItem(withTitle: "Show Quick Help",
+                        action: nil,
+                        keyEquivalent: "",
+                        at: 3)
+
+        menu.insertItem(.separator(), at: 4)
+
+        return menu
+    }
+
+    func codeMenu(_ menu: NSMenu) -> NSMenu {
+
+        menu.insertItem(withTitle: "Refactor",
+                        action: nil,
+                        keyEquivalent: "",
+                        at: 5)
+
+        menu.insertItem(withTitle: "Find",
+                        action: nil,
+                        keyEquivalent: "",
+                        at: 6)
+
+        menu.insertItem(withTitle: "Navigate",
+                        action: nil,
+                        keyEquivalent: "",
+                        at: 7)
+
+        menu.insertItem(.separator(), at: 8)
+
+        return menu
+    }
+
+    func gitMenu(_ menu: NSMenu) -> NSMenu {
+        menu.insertItem(withTitle: "Show Last Change For Line",
+                        action: nil,
+                        keyEquivalent: "",
+                        at: 9)
+
+        menu.insertItem(withTitle: "Create Code Snippet...",
+                        action: nil,
+                        keyEquivalent: "",
+                        at: 10)
+
+        menu.insertItem(withTitle: "Add Pull Request Discussion to Current Line",
+                        action: nil,
+                        keyEquivalent: "",
+                        at: 11)
+
+        menu.insertItem(.separator(), at: 12)
+
+        return menu
+    }
+
+    func removeSubMenus(_ menu: NSMenu) -> NSMenu {
+        // remove unwanted "Font" menu and its submenus
+
+        if let substitutionsItem = menu.item(withTitle: "Substitutions") {
+            menu.removeItem(substitutionsItem)
+        }
+
+        if let transformationsItem = menu.item(withTitle: "Transformations") {
+            menu.removeItem(transformationsItem)
+        }
+
+        if let speechItem = menu.item(withTitle: "Speech") {
+            menu.removeItem(speechItem)
+        }
+
+        if let shareItem = menu.item(withTitle: "Share") {
+            menu.removeItem(shareItem)
+        }
+
+        if let searchGoogleItem = menu.item(withTitle: "Search With Google") {
+            menu.removeItem(searchGoogleItem)
+        }
+
+        if let fontMenuItem = menu.item(withTitle: "Font") {
+            menu.removeItem(fontMenuItem)
+        }
+
+        return menu
+    }
+
+    func removeTextMenu(_ menu: NSMenu) -> NSMenu {
+
+        if let guessesItem = menu.item(withTitle: "No Guesses Found") {
+            menu.removeItem(guessesItem)
+        }
+
+        if let ignoreItem = menu.item(withTitle: "Ignore Spelling") {
+            menu.removeItem(ignoreItem)
+        }
+
+        if let learnItem = menu.item(withTitle: "Learn Spelling") {
+            menu.removeItem(learnItem)
+        }
+
+        if let lookUpItem = menu.item(withTitle: "Look Up") {
+            menu.removeItem(lookUpItem)
+        }
+
+        if let searchGoogleItem = menu.item(withTitle: "Search With Google") {
+            menu.removeItem(searchGoogleItem)
+        }
+
+        if let translateItem = menu.item(withTitle: "Translate") {
+            menu.removeItem(translateItem)
+        }
+
+        if let spellingGrammarItem = menu.item(withTitle: "Spelling and Grammar") {
+            menu.removeItem(spellingGrammarItem)
+        }
+
+        return menu
+    }
+
 }
