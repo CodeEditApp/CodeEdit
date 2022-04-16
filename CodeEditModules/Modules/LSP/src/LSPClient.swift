2d44f050802ae0058b149cac764295ba3a7a6352
Pavel Kasila <pavel.kasila@gmail.com>
2022-04-16 14:24:59 +0300

    Add docs to `LSPClient`

--- CodeEditModules/Modules/LSP/src/LSPClient.swift
+++ CodeEditModules/Modules/LSP/src/LSPClient.swift
@@ -7,17 +7,24 @@
 
 import Foundation
 
+/// A LSP client to handle Language Server process
 public class LSPClient {
     var exec: URL
     var workspace: URL
     var process: Process
 
-    public init(_ exec: URL, workspace: URL, arguments: [String]?) throws {
-        self.exec = exec
+    /// Initialize new LSP client
+    /// - Parameters:
+    ///   - executable: Executable of the Language Server to be run
+    ///   - workspace: Workspace's URL
+    ///   - arguments: Additional arguments from `CELSPArguments` in `Info.plist` of the Language Server bundle
+    public init(_ executable: URL, workspace: URL, arguments: [String]?) throws {
+        self.exec = executable
         self.workspace = workspace
-        self.process = try Process.run(exec, arguments: arguments ?? ["--stdio"], terminationHandler: nil)
+        self.process = try Process.run(executable, arguments: arguments ?? ["--stdio"], terminationHandler: nil)
     }
 
+    /// Close the process
     public func close() {
         self.process.terminate()
     }
