1a7bac69a29ab24ae10ee75f5862f90b1b593d17
Javier Solorzano <javiersolorzano@icloud.com>
2022-04-19 09:27:53 -0500

    f** make inspector and sidebar dock icons selectable and draggable

--- CodeEdit/InspectorSidebar/InspectorSidebarToolbar.swift
+++ CodeEdit/InspectorSidebar/InspectorSidebarToolbar.swift
@@ -10,12 +10,19 @@ import SwiftUI
 struct InspectorSidebarToolbarTop: View {
     @Binding
     var selection: Int
-
+    
+    @State var targeted: Bool = true
+    @State private var icons = [
+        InspectorDockIcon(imageName: "doc", title: "File Inspector", id: 0),
+        InspectorDockIcon(imageName: "clock", title: "History Inspector", id: 1),
+        InspectorDockIcon(imageName: "questionmark.circle", title: "Quick Help Inspector", id: 2)
+    ]
+    
     var body: some View {
         HStack(spacing: 10) {
-            icon(systemImage: "doc", title: "File Inspector", id: 0)
-            icon(systemImage: "clock", title: "History Inspector", id: 1)
-            icon(systemImage: "questionmark.circle", title: "Quick Help Inspector", id: 2)
+            ForEach(icons) { icon in
+                makeInspectorIcon(systemImage: icon.imageName, title: icon.title, id: icon.id)
+            }
         }
         .frame(height: 29, alignment: .center)
         .frame(maxWidth: .infinity)
@@ -29,7 +36,7 @@ struct InspectorSidebarToolbarTop: View {
             .foregroundColor(Color("InspectorBackgroundColor")))
     }
 
-    func icon(systemImage: String, title: String, id: Int) -> some View {
+    func makeInspectorIcon(systemImage: String, title: String, id: Int) -> some View {
         Button {
             selection = id
         } label: {
@@ -38,9 +45,38 @@ struct InspectorSidebarToolbarTop: View {
                 .symbolVariant(id == selection ? .fill : .none)
                 .foregroundColor(id == selection ? .accentColor : .secondary)
                 .frame(width: 16, alignment: .center)
+                .onDrop(of: [.utf8PlainText], isTargeted: nil) { providers in
+                    guard let provider = providers.first else { return false }
+                    provider.loadItem(forTypeIdentifier: "public.utf8-plain-text", options: nil) { data, error in
+                        if let data = data as? Data, let name = String(data: data, encoding: .utf8), let movedIndex = icons.firstIndex(where: { $0.imageName == name }), let insertionIndex = icons.firstIndex (where: { $0.imageName == systemImage }) {
+                            let tempIcon = icons[movedIndex]
+                            icons.remove(at: movedIndex)
+                            icons.insert(tempIcon, at: insertionIndex)
+                        }
+                    }
+                    return false
+                }
+                .onDrag {
+                    return .init(object: NSString(string: systemImage))
+                }
         }
         .buttonStyle(.plain)
     }
+    
+    private func getSafeImage(named: String, accesibilityDescription: String?) -> Image {
+        if let nsImage = NSImage(systemSymbolName: named, accessibilityDescription: accesibilityDescription) {
+            return Image(nsImage: nsImage)
+        } else {
+            return Image(symbol: named)
+        }
+    }
+    
+    private struct InspectorDockIcon: Identifiable {
+        let imageName: String
+        let title: String
+        var id: Int
+    }
+    
 }
 
 struct InspectorSidebarToolbar_Previews: PreviewProvider {
--- CodeEdit/NavigatorSidebar/NavigatorSidebarToolbarTop.swift
+++ CodeEdit/NavigatorSidebar/NavigatorSidebarToolbarTop.swift
@@ -7,47 +7,30 @@
 
 import SwiftUI
 import CodeEditSymbols
-import AppKit
 
 struct NavigatorSidebarToolbarTop: View {
     @Environment(\.controlActiveState)
     var activeState
 
-    typealias iconInfo = (imageName: String, title: String, id: Int)
     @State private var icons = [
-        DockIcon(imageName: "folder", title: "Project", id: 0),
-        DockIcon(imageName: "vault", title: "Version Control", id: 1),
-        DockIcon(imageName: "magnifyingglass", title: "Search", id: 2),
-        DockIcon(imageName: "shippingbox", title: "...", id: 3),
-        DockIcon(imageName: "play", title: "...", id: 4),
-        DockIcon(imageName: "exclamationmark.triangle", title: "...", id: 5),
-        DockIcon(imageName: "curlybraces.square", title: "...", id: 6),
-        DockIcon(imageName: "puzzlepiece.extension", title: "...", id: 7),
-        DockIcon(imageName: "square.grid.2x2", title: "...", id: 8)
+        SidebarDockIcon(imageName: "folder", title: "Project", id: 0),
+        SidebarDockIcon(imageName: "vault", title: "Version Control", id: 1),
+        SidebarDockIcon(imageName: "magnifyingglass", title: "Search", id: 2),
+        SidebarDockIcon(imageName: "shippingbox", title: "...", id: 3),
+        SidebarDockIcon(imageName: "play", title: "...", id: 4),
+        SidebarDockIcon(imageName: "exclamationmark.triangle", title: "...", id: 5),
+        SidebarDockIcon(imageName: "curlybraces.square", title: "...", id: 6),
+        SidebarDockIcon(imageName: "puzzlepiece.extension", title: "...", id: 7),
+        SidebarDockIcon(imageName: "square.grid.2x2", title: "...", id: 8)
     ]
     
     @Binding
     var selection: Int
-    @State var targeted: Bool = true
     
     var body: some View {
         HStack(spacing: 2) {
             ForEach(icons) { icon in
                 makeIcon(named: icon.imageName, title: icon.title, id: icon.id)
-                    .onDrop(of: [.utf8PlainText], isTargeted: self.$targeted) { providers in
-                        guard let provider = providers.first else { return false }
-                        provider.loadItem(forTypeIdentifier: "public.utf8-plain-text", options: nil) { data, error in
-                            if let data = data as? Data, let name = String(data: data, encoding: .utf8), let movedIndex = icons.firstIndex(where: { $0.imageName == name }), let insertionIndex = icons.firstIndex (where: { $0.imageName == icon.imageName }) {
-                                let tempIcon = icons[movedIndex]
-                                icons.remove(at: movedIndex)
-                                icons.insert(tempIcon, at: insertionIndex)
-                            }
-                        }
-                        return false
-                    }
-                    .onDrag {
-                        return NSItemProvider(object: NSString(string: icon.imageName))
-                    }
             }
         }
         .frame(height: 29, alignment: .center)
@@ -70,24 +53,31 @@ struct NavigatorSidebarToolbarTop: View {
               id: Int,
               scale: Image.Scale = .medium
     ) -> some View {
-        getSafeImage(named: named, accesibilityDescription: title)
-            .help(title)
-            .frame(width: 25, height: 25, alignment: .center)
-            .onAppear {
-                selection = id
-            }
-            
-//        Button {
-//            selection = id
-//        } label: {
-//            getSafeImage(named: named, accesibilityDescription: title)
-//                .help(title)
-//        }
-        //.buttonStyle(NavigatorToolbarButtonStyle(id: id, selection: selection, activeState: activeState))
-        //.imageScale(scale)
+        Button {
+            selection = id
+        } label: {
+            getSafeImage(named: named, accesibilityDescription: title)
+                .help(title)
+                .onDrop(of: [.utf8PlainText], isTargeted: nil) { providers in
+                    guard let provider = providers.first else { return false }
+                    provider.loadItem(forTypeIdentifier: "public.utf8-plain-text", options: nil) { data, error in
+                        if let data = data as? Data, let name = String(data: data, encoding: .utf8), let movedIndex = icons.firstIndex(where: { $0.imageName == name }), let insertionIndex = icons.firstIndex (where: { $0.imageName == named }) {
+                            let tempIcon = icons[movedIndex]
+                            icons.remove(at: movedIndex)
+                            icons.insert(tempIcon, at: insertionIndex)
+                        }
+                    }
+                    return false
+                }
+                .onDrag {
+                    return .init(object: NSString(string: named))
+                }
+        }
+        .buttonStyle(NavigatorToolbarButtonStyle(id: id, selection: selection, activeState: activeState))
+        .imageScale(scale)
     }
     
-    func getSafeImage(named: String, accesibilityDescription: String?) -> Image {
+    private func getSafeImage(named: String, accesibilityDescription: String?) -> Image {
         if let nsImage = NSImage(systemSymbolName: named, accessibilityDescription: accesibilityDescription) {
             return Image(nsImage: nsImage)
         } else {
@@ -110,17 +100,7 @@ struct NavigatorSidebarToolbarTop: View {
         }
     }
     
-    private struct DockIcon: Identifiable, DropDelegate {
-        func performDrop(info: DropInfo) -> Bool {
-            guard let provider = info.itemProviders(for: [.utf8PlainText]).first else { return false }
-            provider.loadItem(forTypeIdentifier: "public.utf8-plain-text", options: nil) { data, error in
-                if let data = data as? Data, let name = String(data: data, encoding: .utf8) {
-                    print(name)
-                }
-            }
-            return false
-        }
-        
+    private struct SidebarDockIcon: Identifiable {
         let imageName: String
         let title: String
         var id: Int
