1218756a200b277a864f19ac8e33ca5e2ef099ed
Stef Kors <stef.kors@gmail.com>
2022-04-19 12:07:24 +0300

    Swap safeAreaInset for ZStack

--- CodeEditModules/Modules/StatusBar/src/StatusBarItems/StatusBarDrawer.swift
+++ CodeEditModules/Modules/StatusBar/src/StatusBarItems/StatusBarDrawer.swift
@@ -28,24 +28,24 @@ internal struct StatusBarDrawer: View {
         }
         return 0
     }
-
+    
     internal var body: some View {
-        TerminalEmulatorView(url: model.workspaceURL)
-            .frame(minHeight: 0,
-                   idealHeight: height,
-                   maxHeight: height)
-            .safeAreaInset(edge: .bottom) {
-                HStack(alignment: .center, spacing: 10) {
-                    FilterTextField(title: "Filter", text: $searchText)
-                        .frame(maxWidth: 300)
-                    Spacer()
-                    StatusBarClearButton(model: model)
-                    Divider()
-                    StatusBarSplitTerminalButton(model: model)
-                    StatusBarMaximizeButton(model: model)
-                }
-                .padding(.all, 10)
-                .frame(maxHeight: 34)
+        ZStack(alignment: .bottom) {
+            TerminalEmulatorView(url: model.workspaceURL)
+                .frame(minHeight: 0,
+                       idealHeight: height,
+                       maxHeight: height)
+            HStack(alignment: .center, spacing: 10) {
+                FilterTextField(title: "Filter", text: $searchText)
+                    .frame(maxWidth: 300)
+                Spacer()
+                StatusBarClearButton(model: model)
+                Divider()
+                StatusBarSplitTerminalButton(model: model)
+                StatusBarMaximizeButton(model: model)
             }
+            .padding(.all, 10)
+            .frame(maxHeight: 34)
+        }
     }
 }
