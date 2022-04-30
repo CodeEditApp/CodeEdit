6ca29a6c11cf656510cb4142d98579f4ced4f687
Angelk90 <angelo.k90@hotmail.it>
2022-04-30 09:53:51 +0200

    Fix color

--- CodeEdit/NavigatorSidebar/FindNavigator/FindNavigatorSearchBar.swift
+++ CodeEdit/NavigatorSidebar/FindNavigator/FindNavigatorSearchBar.swift
@@ -8,15 +8,6 @@
 import SwiftUI
 import CodeEditUI
 
-extension Color {
-    init(hex: Int, opacity: Double = 1.0) {
-        let red = Double((hex & 0xff0000) >> 16) / 255.0
-        let green = Double((hex & 0xff00) >> 8) / 255.0
-        let blue = Double((hex & 0xff) >> 0) / 255.0
-        self.init(.sRGB, red: red, green: green, blue: blue, opacity: opacity)
-    }
-}
-
 struct FindNavigatorSearchBar: View {
     @Environment(\.colorScheme)
     var colorScheme
