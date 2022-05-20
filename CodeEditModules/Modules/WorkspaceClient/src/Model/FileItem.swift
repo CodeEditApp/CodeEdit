c15197302b622989e87c6006cebe747daa3980c8
Nanashi Li <tihan-nico@plexus.dev>
2022-05-20 14:26:21 +0200

    Source Control Navigator: Added support for viewing file changes

--- CodeEditModules/Modules/WorkspaceClient/src/Model/FileItem.swift
+++ CodeEditModules/Modules/WorkspaceClient/src/Model/FileItem.swift
@@ -94,7 +94,7 @@ public extension WorkspaceClient {
         public var systemImage: String {
             switch children {
             case nil:
-                return fileIcon
+                return FileIcon.fileIcon(fileType: fileType)
             case let .some(children):
                 return folderIcon(children)
             }
@@ -106,8 +106,23 @@ public extension WorkspaceClient {
         }
 
         /// Returns the extension of the file or an empty string if no extension is present.
-        public var fileType: String {
-            url.pathExtension
+        public var fileType: FileIcon.FileType {
+            .init(rawValue: url.pathExtension) ?? .txt
+        }
+
+        /// Returns a string describing a SFSymbol for folders
+        ///
+        /// If it is the top-level folder this will return `"square.dashed.inset.filled"`.
+        /// If it is a `.codeedit` folder this will return `"folder.fill.badge.gearshape"`.
+        /// If it has children this will return `"folder.fill"` otherwise `"folder"`.
+        private func folderIcon(_ children: [FileItem]) -> String {
+            if self.parent == nil {
+                return "square.dashed.inset.filled"
+            }
+            if self.fileName == ".codeedit" {
+                return "folder.fill.badge.gearshape"
+            }
+            return children.isEmpty ? "folder" : "folder.fill"
         }
 
         /// Returns the file name with optional extension (e.g.: `Package.swift`)
@@ -120,103 +135,11 @@ public extension WorkspaceClient {
             try? url.resourceValues(forKeys: [.contentTypeKey]).contentType
         }
 
-        /// Returns a string describing a SFSymbol for folders
-        ///
-        /// If it is the top-level folder this will return `"square.dashed.inset.filled"`.
-        /// If it is a `.codeedit` folder this will return `"folder.fill.badge.gearshape"`.
-        /// If it has children this will return `"folder.fill"` otherwise `"folder"`.
-        private func folderIcon(_ children: [FileItem]) -> String {
-            if parent == nil {
-                return "square.dashed.inset.filled"
-            }
-            if fileName == ".codeedit" {
-                return "folder.fill.badge.gearshape"
-            }
-            return children.isEmpty ? "folder" : "folder.fill"
-        }
-
-        /// Returns a string describing a SFSymbol for files
-        ///
-        /// If not specified otherwise this will return `"doc"`
-        private var fileIcon: String {
-            switch fileType {
-            case "json", "js":
-                return "curlybraces"
-            case "css":
-                return "number"
-            case "jsx":
-                return "atom"
-            case "swift":
-                return "swift"
-            case "env", "example":
-                return "gearshape.fill"
-            case "gitignore":
-                return "arrow.triangle.branch"
-            case "png", "jpg", "jpeg", "ico":
-                return "photo"
-            case "svg":
-                return "square.fill.on.circle.fill"
-            case "entitlements":
-                return "checkmark.seal"
-            case "plist":
-                return "tablecells"
-            case "md", "txt", "rtf":
-                return "doc.plaintext"
-            case "html", "py", "sh":
-                return "chevron.left.forwardslash.chevron.right"
-            case "LICENSE":
-                return "key.fill"
-            case "java":
-                return "cup.and.saucer"
-            case "h":
-                return "h.square"
-            case "m":
-                return "m.square"
-            case "vue":
-                return "v.square"
-            case "go":
-                return "g.square"
-            case "sum":
-                return "s.square"
-            case "mod":
-                return "m.square"
-            case "Makefile":
-                return "terminal"
-            default:
-                return "doc"
-            }
-        }
-
         /// Returns a `Color` for a specific `fileType`
         ///
         /// If not specified otherwise this will return `Color.accentColor`
         public var iconColor: Color {
-            switch fileType {
-            case "swift", "html":
-                return .orange
-            case "java":
-                return .red
-            case "js", "entitlements", "json", "LICENSE":
-                return Color("SidebarYellow")
-            case "css", "ts", "jsx", "md", "py":
-                return .blue
-            case "sh":
-                return .green
-            case "vue":
-                return Color(red: 0.255, green: 0.722, blue: 0.514, opacity: 1.000)
-            case "h":
-                return Color(red: 0.667, green: 0.031, blue: 0.133, opacity: 1.000)
-            case "m":
-                return Color(red: 0.271, green: 0.106, blue: 0.525, opacity: 1.000)
-            case "go":
-                return Color(red: 0.02, green: 0.675, blue: 0.757, opacity: 1.0)
-            case "sum", "mod":
-                return Color(red: 0.925, green: 0.251, blue: 0.478, opacity: 1.0)
-            case "Makefile":
-                return Color(red: 0.937, green: 0.325, blue: 0.314, opacity: 1.0)
-            default:
-                return .accentColor
-            }
+            FileIcon.iconColor(fileType: fileType)
         }
 
         // MARK: Statics
