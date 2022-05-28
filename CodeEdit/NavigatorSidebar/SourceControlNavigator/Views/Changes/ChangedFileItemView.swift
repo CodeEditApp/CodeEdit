07b942c4d31473d37d63641c5c73c922b2a7d7aa
Lingxi Li <lilingxi01@gmail.com>
2022-05-28 09:18:01 -0400

    [SourceControl] Fix indentation issue.
    All lines are having one additional unneeded space at the front.

--- CodeEdit/NavigatorSidebar/SourceControlNavigator/Views/Changes/ChangedFileItemView.swift
+++ CodeEdit/NavigatorSidebar/SourceControlNavigator/Views/Changes/ChangedFileItemView.swift
@@ -6,67 +6,67 @@
 //
 
 import SwiftUI
- import Git
+import Git
 
- struct ChangedFileItemView: View {
+struct ChangedFileItemView: View {
+    @State
+    var changedFile: ChangedFile
 
-     @State
-     var changedFile: ChangedFile
+    @Binding
+    var selection: ChangedFile.ID?
 
-     @Binding
-     var selection: ChangedFile.ID?
+    @State
+    var workspaceURL: URL
 
-     @State
-     var workspaceURL: URL
+    var body: some View {
+        HStack {
+            Image(systemName: changedFile.systemImage)
+                .frame(width: 12, height: 12)
+                .foregroundColor(selection == changedFile.id ? .white : changedFile.iconColor)
 
-     var body: some View {
-         HStack {
-             Image(systemName: changedFile.systemImage)
-                 .frame(width: 11, height: 11)
-                 .foregroundColor(selection == changedFile.id ? .white : changedFile.iconColor)
+            Text(changedFile.fileName)
+                .font(.system(size: 12))
+                .foregroundColor(selection == changedFile.id ? .white : .secondary)
 
-             Text(changedFile.fileName)
-                 .font(.system(size: 11))
-                 .foregroundColor(selection == changedFile.id ? .white : .secondary)
+            Spacer()
 
-             Text(changedFile.changeTypeValue)
-                 .font(.system(size: 11))
-                 .foregroundColor(selection == changedFile.id ? .white : .secondary)
-                 .frame(maxWidth: .infinity, alignment: .trailing)
-         }
-         .contextMenu {
-             Group {
-                 Button("View in Finder") {
-                     changedFile.showInFinder(workspaceURL: workspaceURL)
-                 }
-                 Button("Reveal in Project Navigator") {}
-                     .disabled(true) // TODO: Implementation Needed
-                 Divider()
-             }
-             Group {
-                 Button("Open in New Tab") {}
-                     .disabled(true) // TODO: Implementation Needed
-                 Button("Open in New Window") {}
-                     .disabled(true) // TODO: Implementation Needed
-                 Button("Open with External Editor") {}
-                     .disabled(true) // TODO: Implementation Needed
-             }
-             Group {
-                 Divider()
-                 Button("Commit \(changedFile.fileName)...") {}
-                     .disabled(true) // TODO: Implementation Needed
-                 Divider()
-                 Button("Discard Changes in \(changedFile.fileName)...") {}
-                     .disabled(true) // TODO: Implementation Needed
-                 Divider()
-             }
-             Group {
-                 Button("Add \(changedFile.fileName)") {}
-                     .disabled(true) // TODO: Implementation Needed
-                 Button("Mark \(changedFile.fileName) as Resolved") {}
-                     .disabled(true) // TODO: Implementation Needed
-             }
-         }
-         .padding(.leading, 15)
-     }
- }
+            Text(changedFile.changeTypeValue)
+                .font(.system(size: 12))
+                .foregroundColor(selection == changedFile.id ? .white : .secondary)
+        }
+        .contextMenu {
+            Group {
+                Button("View in Finder") {
+                    changedFile.showInFinder(workspaceURL: workspaceURL)
+                }
+                Button("Reveal in Project Navigator") {}
+                    .disabled(true) // TODO: Implementation Needed
+                Divider()
+            }
+            Group {
+                Button("Open in New Tab") {}
+                    .disabled(true) // TODO: Implementation Needed
+                Button("Open in New Window") {}
+                    .disabled(true) // TODO: Implementation Needed
+                Button("Open with External Editor") {}
+                    .disabled(true) // TODO: Implementation Needed
+            }
+            Group {
+                Divider()
+                Button("Commit \(changedFile.fileName)...") {}
+                    .disabled(true) // TODO: Implementation Needed
+                Divider()
+                Button("Discard Changes in \(changedFile.fileName)...") {}
+                    .disabled(true) // TODO: Implementation Needed
+                Divider()
+            }
+            Group {
+                Button("Add \(changedFile.fileName)") {}
+                    .disabled(true) // TODO: Implementation Needed
+                Button("Mark \(changedFile.fileName) as Resolved") {}
+                    .disabled(true) // TODO: Implementation Needed
+            }
+        }
+        .padding(.leading, 15)
+    }
+}
