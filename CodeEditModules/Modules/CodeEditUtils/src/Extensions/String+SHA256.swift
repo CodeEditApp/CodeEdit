054a920f2ab81318a4898e1cd534e87b5c1a8fd7
Lukas Pistrol <lukas@pistrol.com>
2022-06-10 10:26:28 +0200

    refactor sha256 & md5 string extension

--- CodeEditModules/Modules/CodeEditUtils/src/Extensions/String+SHA256.swift
+++ CodeEditModules/Modules/CodeEditUtils/src/Extensions/String+SHA256.swift
@@ -1,8 +1,8 @@
 //
-//  String+MD5.swift
+//  String+SHA256.swift
 //  CodeEditModules/CodeEditUtils
 //
-//  Created by Nanashi Li on 2022/04/19.
+//  Created by Debdut Karmakar on 6/9/22.
 //
 
 import Foundation
@@ -10,24 +10,24 @@ import CryptoKit
 
 public extension String {
 
-    /// Returns a MD5 encrypted String of the input String
+    /// Returns a SHA256 encrypted String of the input String
     ///
     /// - Parameters:
     ///   - trim: If `true` the input string will be trimmed from whitespaces and new-lines. Defaults to `false`.
     ///   - caseSensitive: If `false` the input string will be converted to lowercase characters. Defaults to `true`.
     /// - Returns: A String in HEX format
-    func md5(trim: Bool = false, caseSensitive: Bool = true) -> String {
+    func sha256(trim: Bool = false, caseSensitive: Bool = true) -> String {
         var string = self
 
-        // trim whitespaces & new lines if specifiedå
-        if trim { string = string.trimmingCharacters(in: .whitespacesAndNewlines)}
+        // trim whitespaces & new lines if specified
+        if trim { string = string.trimmingCharacters(in: .whitespacesAndNewlines) }
 
         // make string lowercased if not case sensitive
         if !caseSensitive { string = string.lowercased() }
 
         // compute the hash
         // (note that `String.data(using: .utf8)!` is safe since it will never fail)
-        let computed = Insecure.MD5.hash(data: string.data(using: .utf8)!)
+        let computed = SHA256.hash(data: string.data(using: .utf8)!)
 
         // map the result to a hex string and return
         return computed.compactMap { String(format: "%02x", $0) }.joined()
