32f25d946beca34fa3d543c35e6a3ec333e71939
Shibo Tong <shibo0617@gmail.com>
2022-07-10 18:05:24 +1000

    update preview item in OtherFileView

--- CodeEditModules/Modules/CodeFile/src/Other/OtherFileView.swift
+++ CodeEditModules/Modules/CodeFile/src/Other/OtherFileView.swift
@@ -16,7 +16,6 @@ import QuickLookUI
 /// ```
 public struct OtherFileView: NSViewRepresentable {
 
-    @ObservedObject
     private var otherFile: CodeFileDocument
 
     /// Initialize the OtherFileView
@@ -29,11 +28,13 @@ public struct OtherFileView: NSViewRepresentable {
 
     public func makeNSView(context: Context) -> QLPreviewView {
         let qlPreviewView = QLPreviewView()
-        qlPreviewView.previewItem = otherFile
+        qlPreviewView.previewItem = otherFile.previewItemURL as QLPreviewItem
         return qlPreviewView
     }
 
+    /// Update preview file when file changed
     public func updateNSView(_ nsView: QLPreviewView, context: Context) {
-        
+        nsView.previewItem = otherFile.previewItemURL as QLPreviewItem
     }
+
 }
