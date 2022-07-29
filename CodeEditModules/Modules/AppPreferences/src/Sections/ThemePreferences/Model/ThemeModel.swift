73fd230ae107e21344c2a5ffdb537f8290286cd3
Adam S <asalaymeh@gmail.com>
2022-07-29 01:29:56 -0400

    Added Midnight theme

--- CodeEditModules/Modules/AppPreferences/src/Sections/ThemePreferences/Model/ThemeModel.swift
+++ CodeEditModules/Modules/AppPreferences/src/Sections/ThemePreferences/Model/ThemeModel.swift
@@ -153,7 +153,9 @@ public final class ThemeModel: ObservableObject {
             "codeedit-xcode-dark",
             "codeedit-xcode-light",
             "codeedit-github-dark",
-            "codeedit-github-light"
+            "codeedit-github-light",
+            "codeedit-midnight"
+            
         ]
         for themeName in bundledThemeNames {
             guard let defaultUrl = Bundle.main.url(forResource: themeName, withExtension: "json") else {
