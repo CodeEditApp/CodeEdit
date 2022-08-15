a8d0f3050124bde50a86a957a9781afbf5f55033
Alex Sinelnikov <sinelniko@gmail.com>
2022-08-15 08:21:55 +0300

    Changed view items to be buttons instead of VStacks

--- CodeEdit/Commands/CommandPaletteView.swift
+++ CodeEdit/Commands/CommandPaletteView.swift
@@ -20,6 +20,10 @@ public struct CommandPaletteView: View {
     public init(state: CommandPaletteState, closePalette: @escaping () -> Void) {
         self.state = state
         self.closePalette = closePalette
+        print(commandsList.count)
+        print(commandsList[0])
+        self.selectedItem = commandManager.commands[0]
+
     }
 
     func resetState() {
@@ -77,21 +81,23 @@ public struct CommandPaletteView: View {
     /// It should return bool value in order to notify underlying handler if event was handled or not.
     /// So returning true - means you need to break the chain and do not pass event down the line
     func onKeyDown(with event: NSEvent) -> Bool {
-        if event.keyCode == 125 {
+
+        switch event.keyCode {
+        case 125:
             selectNext()
             return true
-        }
-
-        if event.keyCode == 126 {
+        case 126:
             selectPrev()
             return true
-        }
-
-        if event.keyCode == 36 {
+        case 36:
             if let command = self.selectedItem {
                 callHandler(command: command)
             }
             return true
+        case 53:
+            closePalette()
+        default:
+            return false
         }
 
         return false
@@ -102,13 +108,18 @@ public struct CommandPaletteView: View {
         state.fetchMatchingCommands(val: text)
     }
 
+    func onCommandClick(command: Command) {
+        self.selectedItem = command
+        callHandler(command: command)
+    }
+
     public var body: some View {
         VStack(spacing: 0.0) {
                 HStack(alignment: .center, spacing: 0) {
                     Image(systemName: "command")
                         .resizable()
                         .scaledToFit()
-                        .frame(width: 20, height: 20)
+                        .frame(width: 16, height: 16)
                         .padding(.leading, 20)
                         .offset(x: 0, y: 1)
 //                    TextField("Search Commands", text: $state.commandQuery)
@@ -130,7 +141,7 @@ public struct CommandPaletteView: View {
                                      onTextChange: onQueryChange, text: $state.commandQuery)
                         .font(.system(size: 24, weight: .light, design: .default))
                         .padding(16)
-                        .frame(height: 60, alignment: .center)
+                        .frame(height: 52, alignment: .center)
                         .foregroundColor(Color(.systemGray).opacity(0.85))
                         .background(EffectView(.sidebar, blendingMode: .behindWindow))
                 }
@@ -138,22 +149,25 @@ public struct CommandPaletteView: View {
             Divider()
             VStack(spacing: 0) {
                 List(commandsList, selection: $state.selected) { command in
-                        VStack(alignment: .leading, spacing: 0) {
-                            Text(command.title).foregroundColor(Color.white)
-                                .padding(EdgeInsets.init(top: 0, leading: 10, bottom: 0, trailing: 0))
-                                .frame(height: 10)
+                    // swiftlint:disable multiple_closures_with_trailing_closure
+                    Button(action: { onCommandClick(command: command) }) {
+                        VStack {
+                            Text(command.title)
+                            .padding(EdgeInsets.init(top: 0, leading: 8, bottom: 0, trailing: 0))
+                            .foregroundColor(.white)
                         }.frame(maxWidth: .infinity, maxHeight: 15, alignment: .leading)
-                            .listRowBackground(self.selectedItem == command ?
-                                               RoundedRectangle(cornerRadius: 5, style: .continuous)
-                                .fill(Color(.systemBlue)) :
-                                                RoundedRectangle(cornerRadius: 5, style: .continuous)
-                                .fill(Color.clear) )
-
-                        .onTapGesture {
-                            self.selectedItem = command
-                            callHandler(command: command)
-                        }.onHover(perform: { _ in self.selectedItem = command })
+                    }.frame(maxWidth: .infinity, maxHeight: 15, alignment: .leading)
+                        .listRowInsets(EdgeInsets.init(top: 0, leading: 0, bottom: 0, trailing: 0))
+                        .padding(EdgeInsets.init(top: 5, leading: 5, bottom: 5, trailing: 0))
+                        .buttonStyle(.borderless)
+                        .background(self.selectedItem == command ?
+                                           RoundedRectangle(cornerRadius: 5, style: .continuous)
+                            .fill(Color(red: 0, green: 0.38, blue: 0.816, opacity: 0.85)) :
+                                            RoundedRectangle(cornerRadius: 5, style: .continuous)
+                            .fill(Color.clear))
+                        .onHover(perform: { _ in self.selectedItem = command })
                     }.listStyle(SidebarListStyle())
+
             }
         }
         .background(EffectView(.sidebar, blendingMode: .behindWindow))
@@ -194,15 +208,6 @@ class ActionAwareInputView: NSTextView, NSTextFieldDelegate {
             return
         }
 
-//        if event.keyCode == 125 {
-//            onDown?()
-//            return
-//        }
-//
-//        if event.keyCode == 126 {
-//            onUp?()
-//            return
-//        }
         super.keyDown(with: event)
     }
 
@@ -229,7 +234,7 @@ public struct ActionAwareInput: NSViewRepresentable {
         input.onTextChange = onTextChange
         input.string = text
         input.onDown = onDown
-        input.font = .systemFont(ofSize: 22)
+        input.font = .systemFont(ofSize: 20, weight: .light)
         input.textColor = NSColor(fontColor)
         input.drawsBackground = false
         input.becomeFirstResponder()
@@ -239,7 +244,8 @@ public struct ActionAwareInput: NSViewRepresentable {
     }
 
     public func updateNSView(_ nsView: NSViewType, context: Context) {
-        print("updated")
         nsView.textContainer?.textView?.string = text
+        // This way we can update light/dark mode font color
+        nsView.textContainer?.textView?.textColor = NSColor(fontColor)
     }
 }
