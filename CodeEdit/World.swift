119584481059c325456d8cc867a1be13f53f2fcf
austincondiff <austincondiff@users.noreply.github.com>
2022-10-04 20:43:52 +0000

    Deploying to gh-pages from  @ 068b4dd9374c42017fde486d74693f5082fde3ac 🚀

--- CodeEdit/World.swift
+++
@@ -1,9 +0,0 @@
-import ShellClient
-
-// swiftlint:disable:next identifier_name
-var Current: World = .init()
-
-// Inspired by: https://vimeo.com/291588126
-struct World {
-    var shellClient: ShellClient = .live()
-}
--- CodeEditModules/README.md
+++
@@ -1,3 +0,0 @@
-# CodeEditModules
-
-`CodeEditModules` is a local Swift Package that contains modules of the app
