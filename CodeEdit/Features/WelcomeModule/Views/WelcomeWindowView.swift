2dd89e1520c0f72d9cdade82629afb124a9dcf69
Matthijs Eikelenboom <matthijs697@live.nl>
2022-11-27 22:01:17 +0100

    Refactored the folder structure in the app

--- CodeEdit/Features/WelcomeModule/Views/WelcomeWindowView.swift
+++ CodeEdit/Features/WelcomeModule/Views/WelcomeWindowView.swift
@@ -1,13 +1,45 @@
 //
-//  Welcome.swift
-//  CodeEdit
+//  WelcomeWindowView.swift
+//  CodeEditModules/WelcomeModule
 //
-//  Created by Nazar Rudnyk on 06.06.2022.
+//  Created by Ziyuan Zhao on 2022/3/18.
 //
 
 import SwiftUI
 
-extension WelcomeWindowView {
+struct WelcomeWindowView: View {
+    private let openDocument: (URL?, @escaping () -> Void) -> Void
+    private let newDocument: () -> Void
+    private let dismissWindow: () -> Void
+    private let shellClient: ShellClient
+
+    init(
+        shellClient: ShellClient,
+        openDocument: @escaping (URL?, @escaping () -> Void) -> Void,
+        newDocument: @escaping () -> Void,
+        dismissWindow: @escaping () -> Void
+    ) {
+        self.shellClient = shellClient
+        self.openDocument = openDocument
+        self.newDocument = newDocument
+        self.dismissWindow = dismissWindow
+    }
+
+    var body: some View {
+        HStack(spacing: 0) {
+            WelcomeView(
+                shellClient: shellClient,
+                openDocument: openDocument,
+                newDocument: newDocument,
+                dismissWindow: dismissWindow
+            )
+            RecentProjectsView(
+                openDocument: openDocument,
+                dismissWindow: dismissWindow
+            )
+        }
+        .edgesIgnoringSafeArea(.top)
+    }
 
     /// Helper function which opens welcome view
     /// TODO: Move this to WelcomeModule after CodeEditDocumentController is in separate module
