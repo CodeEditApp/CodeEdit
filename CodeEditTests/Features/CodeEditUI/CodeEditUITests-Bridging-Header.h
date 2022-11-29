5cd8516959f8902d6183cb2b9876f912093f6ec8
Matthijs Eikelenboom <matthijs697@live.nl>
2022-11-29 12:18:49 +0100

    Test patches

---
+++ CodeEditTests/Features/CodeEditUI/CodeEditUITests-Bridging-Header.h
@@ -0,0 +1,12 @@
+//
+//  CodeEditUITests-Bridging-Header.h
+//  CodeEditUITests
+//
+//  Created by Matthijs Eikelenboom on 29/11/2022.
+//
+
+#ifndef CodeEditUITests_Bridging_Header_h
+#define CodeEditUITests_Bridging_Header_h
+
+
+#endif /* CodeEditUITests_Bridging_Header_h */
---
+++ CodeEditTests/Features/Welcome/WelcomeTests-Bridging-Header.h
@@ -0,0 +1,12 @@
+//
+//  WelcomeTests-Bridging-Header.h
+//  CodeEditUITests
+//
+//  Created by Matthijs Eikelenboom on 29/11/2022.
+//
+
+#ifndef WelcomeTests_Bridging_Header_h
+#define WelcomeTests_Bridging_Header_h
+
+
+#endif /* WelcomeTests_Bridging_Header_h */
