ff06ec8f798f36dbac916f4d42b92fdd98c92ae4
Ben Palmer <benj.c.palmer@gmail.com>
2023-01-03 18:47:24 -0500

    pass bindings for prefs into CodeEditTextView

--- CodeEdit/Features/CodeFile/CodeFileView.swift
+++ CodeEdit/Features/CodeFile/CodeFileView.swift
@@ -70,6 +70,8 @@ struct CodeFileView: View {
             font: $font,
             tabWidth: $prefs.preferences.textEditing.defaultTabWidth,
             lineHeight: .constant(1.2), // TODO: Add to preferences
+            wrapLines: $prefs.preferences.textEditing.wrapLinesToEditorWidth,
+            wrappedIndent: $prefs.preferences.textEditing.spacesToIndentWrappedLines,
             cursorPosition: codeFile.$cursorPosition
         )
         .id(codeFile.fileURL)
