3bf5a997dcf80d0aa26fc87af16673b6efade5f3
Lukas Pistrol <lukas@pistrol.com>
2023-01-20 16:16:33 +0100

    add check that CFBundleShortVersionString exists in Info.plist

---
+++ .github/scripts/test_build_number.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+VERS=$(xcrun agvtool mvers -terse1)
+
+if [ "$VERS" == "" ]; then
+  echo "No version number found"
+  echo "Make sure Info.plist has a CFBundleShortVersionString key"
+  echo ""
+  echo "If Info.plist does not have a CFBundleShortVersionString key, add the following to your Info.plist file:"
+  echo ""
+  echo "<key>CFBundleShortVersionString</key>"
+	echo "<string>0.0.1</string>"
+  exit 1
+else
+  echo "Version number is $VERS"
+  exit 0
+fi
