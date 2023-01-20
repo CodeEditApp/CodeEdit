6ba74ac4adf6896d75457df0348bade27b0750e0
Lukas Pistrol <lukas@pistrol.com>
2023-01-20 16:31:33 +0100

    add comments to script

--- .github/scripts/test_version_number.sh
+++ .github/scripts/test_version_number.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+# get the version number from the Info.plist file using agvtool
 VERS=$(xcrun agvtool mvers -terse1)
 
+# if the version number is empty, exit with an error
 if [ "$VERS" == "" ]; then
   echo "No version number found"
   echo "Make sure Info.plist has a CFBundleShortVersionString key"
@@ -12,8 +14,8 @@ if [ "$VERS" == "" ]; then
   echo "<string>0.0.1</string>"
   echo ""
   echo "For more information see https://github.com/CodeEditApp/CodeEdit/pull/1006"
-  exit 1
+  exit 1 # exit with an error
 else
   echo "Version number is $VERS"
-  exit 0
+  exit 0 # exit with no error
 fi
