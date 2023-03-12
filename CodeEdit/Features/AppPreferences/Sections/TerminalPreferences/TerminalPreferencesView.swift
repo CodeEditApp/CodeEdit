aeae922335ce54148cc1907f25d233d7c3a06683
iggy890 <98705626+iggy890@users.noreply.github.com>
2023-03-12 20:40:54 +0000

    Update TerminalPreferencesView.swift

--- CodeEdit/Features/AppPreferences/Sections/TerminalPreferences/TerminalPreferencesView.swift
+++ CodeEdit/Features/AppPreferences/Sections/TerminalPreferences/TerminalPreferencesView.swift
@@ -26,45 +26,10 @@ struct TerminalPreferencesView: View {
             PreferencesSection("Font") {
                 fontSelector
             }
-            PreferencesSection("Terminal window") {
-                mouseReporting
-                scrollBack
-            }
             PreferencesSection("Cursor") {
                 cursorStyle
                 cursorBlink
             }
-        }
-    }
-
-    /// Only allows integer values in the range of `[100...1,000,000]`
-    private var scrollBackFormatter: NumberFormatter {
-        let formatter = NumberFormatter()
-        formatter.allowsFloats = false
-        formatter.minimum = 100
-        formatter.maximum = 1000000
-
-        return formatter
-    }
-
-    private var scrollBack: some View {
-        HStack(spacing: 5) {
-            TextField("", value: $prefs.preferences.terminal.scrollBack, formatter: scrollBackFormatter)
-                .multilineTextAlignment(.trailing)
-                .frame(width: 40)
-            Stepper(
-                "Scroll back",
-                value: $prefs.preferences.terminal.scrollBack,
-                in: 100...1000000
-            )
-            Text("spaces")
-        }
-    }
-
-    private var mouseReporting: some View {
-        HStack {
-            Toggle("Mouse reporting", isOn: $prefs.preferences.terminal.allowMouseReporting)
-            Text("Allow mouse reporting")
         }
     }
 
