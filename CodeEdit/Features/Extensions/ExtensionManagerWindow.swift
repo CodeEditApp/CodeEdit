950010fe4d8226fd732d0f703dcd1be9475b782f
Wouter01 <wouterhennen@gmail.com>
2023-03-24 00:08:14 +0100

    moved foldermonitor and created extensions window
    Signed-off-by: Wouter01 <wouterhennen@gmail.com>

---
+++ CodeEdit/Features/Extensions/ExtensionManagerWindow.swift
@@ -0,0 +1,20 @@
+//
+//  ExtensionManagerWindow.swift
+//  CodeEdit
+//
+//  Created by Wouter Hennen on 24/03/2023.
+//
+
+import SwiftUI
+
+struct ExtensionManagerWindow: Scene {
+    var body: some Scene {
+        Window("Extensions", id: SceneID.extensions.rawValue) {
+            NavigationSplitView {
+
+            } detail: {
+                
+            }
+        }
+    }
+}
