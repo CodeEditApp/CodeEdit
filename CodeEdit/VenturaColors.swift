8f0cd8ec36ab6f331ae2d4636f6870bf6da45d19
luah5 <raymondvleeshouwer@gmail.com>
2023-03-31 10:05:31 +0100

    add venturacolors.swift

---
+++ CodeEdit/VenturaColors.swift
@@ -0,0 +1,40 @@
+//
+//  VenturaColors.swift
+//  CodeEdit
+//
+//  Created by Raymond Vleeshouwer on 31/03/23.
+//
+
+import SwiftUI
+
+struct VenturaColors {
+    let venturaGray: Color = Color.init(
+        hex: "#B4B4B9",
+        alpha: 1.0
+    )
+
+    let venturaBlue: Color = Color.init(
+        hex: "#5A9FF8",
+        alpha: 1.0
+    )
+
+    let venturaOrange: Color = Color.init(
+        hex: "#EC695C",
+        alpha: 1.0
+    )
+    
+    let venturaPink: Color = Color.init(
+        hex: "#ED6C85",
+        alpha: 1.0
+    )
+
+    let venturaGreen: Color = Color.init(
+        hex: "#82E37C",
+        alpha: 1.0
+    )
+
+    let venturaPurple: Color = Color.init(
+        hex: "#8483EF",
+        alpha: 1.0
+    )
+}
