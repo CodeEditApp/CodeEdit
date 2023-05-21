306d9a7ff6af6f98da6a9027b56acf6e5d21faf7
Austin Condiff <austin.condiff@gmail.com>
2023-05-21 18:20:45 -0500

    Removed unused UI

--- CodeEdit/Features/InspectorSidebar/Views/QuickHelpInspectorView.swift
+++ CodeEdit/Features/InspectorSidebar/Views/QuickHelpInspectorView.swift
@@ -9,29 +9,26 @@ import SwiftUI
 struct QuickHelpInspectorView: View {
     var body: some View {
         VStack(alignment: .leading) {
-
             Text("Quick Help")
                 .foregroundColor(.secondary)
                 .fontWeight(.bold)
                 .font(.system(size: 13))
-                .frame(width: 250, alignment: .leading)
+            VStack {
+                Text("No Quick Help")
+                    .foregroundColor(.secondary)
+                    .font(.system(size: 16))
+                    .fontWeight(.medium)
+                    .padding(.top, 10)
+                    .padding(.bottom, 10)
 
-            Text("No Quick Help")
-                .foregroundColor(.secondary)
-                .font(.system(size: 16))
-                .fontWeight(.medium)
-                .frame(width: 250, alignment: .center)
-                .padding(.top, 10)
-                .padding(.bottom, 10)
-
-            Button("Search Documentation") {
-
-            }.background(in: RoundedRectangle(cornerRadius: 4))
-                .frame(width: 250, alignment: .center)
-                .font(.system(size: 12))
-
+                Button("Search Documentation") {
+                    // Open documentation
+                }
+                .controlSize(.small)
+            }
+            .frame(maxWidth: .infinity)
             Divider().padding(.top, 15)
-
-        }.frame(maxWidth: 250).padding(5)
+        }
+        .frame(maxWidth: .infinity).padding(5)
     }
 }
