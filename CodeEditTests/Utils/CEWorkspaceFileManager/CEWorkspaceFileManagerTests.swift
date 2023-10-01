a47cdc7f653c571c9417f287f76e290298f0826a
Khan Winter <35942988+thecoolwinter@users.noreply.github.com>
2023-10-01 16:00:34 -0500

    Fix dummy file observer

--- CodeEditTests/Utils/CEWorkspaceFileManager/CEWorkspaceFileManagerTests.swift
+++ CodeEditTests/Utils/CEWorkspaceFileManager/CEWorkspaceFileManagerTests.swift
@@ -20,7 +20,7 @@ final class CEWorkspaceFileManagerUnitTests: XCTestCase {
             self.completion = completion
         }
 
-        func fileManagerUpdated() {
+        func fileManagerUpdated(updatedItems: Set<CEWorkspaceFile>) {
             completion?()
         }
     }
