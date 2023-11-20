da2350fcefcd0caca61da47d2297cb4ebf553ddf
Esteban Borai <estebanborai@gmail.com>
2023-11-20 02:28:04 -0300

    feat: discard changes support for `SourceControlToolbarBottom` (#1480)
    * feat: `SourceControlToolbarBottom` discard changes dialog
    
    * feat: discard all changes support for `GitClient`
    
    * fix: swiftlint
    
    * Update SourceControlToolbarBottom.swift
    
    * fix: use `Discard All Changes` text

--- CodeEdit/Features/NavigatorArea/SourceControlNavigator/SourceControlToolbarBottom.swift
+++ CodeEdit/Features/NavigatorArea/SourceControlNavigator/SourceControlToolbarBottom.swift
@@ -8,6 +8,8 @@
 import SwiftUI
 
 struct SourceControlToolbarBottom: View {
+    @EnvironmentObject private var workspace: WorkspaceDocument
+
     var body: some View {
         HStack(spacing: 0) {
             sourceControlMenu
@@ -23,8 +25,11 @@ struct SourceControlToolbarBottom: View {
 
     private var sourceControlMenu: some View {
         Menu {
-            Button("Discard Changes...") {}
-                .disabled(true) // TODO: Implementation Needed
+            Button("Discard All Changes...") {
+                if discardChangesDialog() {
+                    workspace.sourceControlManager?.discardAllChanges()
+                }
+            }
             Button("Stash Changes...") {}
                 .disabled(true) // TODO: Implementation Needed
             Button("Commit...") {}
@@ -37,5 +42,18 @@ struct SourceControlToolbarBottom: View {
         .menuStyle(.borderlessButton)
         .menuIndicator(.hidden)
         .frame(maxWidth: 30)
+    }
+
+    /// Renders a Discard Changes Dialog
+    func discardChangesDialog() -> Bool {
+        let alert = NSAlert()
+
+        alert.messageText = "Do you want to discard all uncommitted, local changes?"
+        alert.informativeText = "This operation cannot be undone."
+        alert.alertStyle = .warning
+        alert.addButton(withTitle: "Discard")
+        alert.addButton(withTitle: "Cancel")
+
+        return alert.runModal() == .alertFirstButtonReturn
     }
 }
