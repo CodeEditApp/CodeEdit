c0a712f43d1d27ae473de6f7c2f796aa946b1d58
Tom Ludwig <83090745+activcoding@users.noreply.github.com>
2023-11-21 16:20:23 +0100

    Added LayzFilePathEnumerator

---
+++ CodeEdit/Features/Documents/LazyFilePathEnumerator.swift
@@ -0,0 +1,42 @@
+//
+//  LazyFilePathEnumerator.swift
+//  CodeEdit
+//
+//  Created by Tommy Ludwig on 21.11.23.
+//
+
+import Foundation
+
+
+/// A class for lazily enumerating file paths in a directory.
+
+/// - Parameters:
+/// - directoryURL: The URL of the directory to enumerate.
+
+/// - Example usage:
+/// ```swift
+/// let directoryURL = URL(fileURLWithPath: "/path/to/your/directory")
+/// let lazyEnumerator = LazyFilePathEnumerator(directoryURL: directoryURL)
+///
+/// while let filePath = lazyEnumerator.getNextFilePath() {
+///     // Process the file path as needed
+///     print("Processing file:", filePath.path)
+///     // Perform your search or other operations on this file path
+/// }
+/// ```
+class LazyFilePathEnumerator {
+    let directoryURL: URL
+    var enumerator: FileManager.DirectoryEnumerator?
+
+    init(directoryURL: URL) {
+        self.directoryURL = directoryURL
+    }
+
+    func getNextFilePath() -> URL? {
+        if enumerator == nil {
+            enumerator = FileManager.default.enumerator(at: directoryURL, includingPropertiesForKeys: nil, options: .skipsHiddenFiles)
+        }
+
+        return enumerator?.nextObject() as? URL
+    }
+}
---
+++ CodeEdit/Features/Documents/LazyStringLoader.swift
@@ -0,0 +1,8 @@
+//
+//  LazyStringLoader.swift
+//  CodeEdit
+//
+//  Created by Tommy Ludwig on 21.11.23.
+//
+
+import Foundation
