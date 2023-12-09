91c43b894bf1e3e42157bb3712ac4825259ea0ad
Tom Ludwig <83090745+activcoding@users.noreply.github.com>
2023-12-09 13:06:16 +0100

    Added doc for TempFolderManager

--- CodeEditTests/Features/Documents/Indexer/TemporaryFile.swift
+++ CodeEditTests/Features/Documents/Indexer/TemporaryFile.swift
@@ -7,7 +7,19 @@
 
 import Foundation
 
+/// A utility class representing a temporary file with automatic cleanup upon deallocation.
+///
+/// This class provides a convenient way to create a temporary
+/// file with a unique name and automatically removes the file when the `TemporaryFile` instance is deallocated.
+///
+/// Example usage:
+/// ```swift
+/// let tempFile = TemporaryFile()
+/// // Use tempFile.url for file operations
+/// // The file will be automatically removed when tempFile is no longer in use.
+/// ```
 class TemporaryFile {
+    /// The URL of the temporary file.
     let url: URL = {
         let folder = NSTemporaryDirectory()
         let name = UUID().uuidString
@@ -15,18 +27,32 @@ class TemporaryFile {
         return NSURL.fileURL(withPathComponents: [folder, name])! as URL
     }()
 
+    /// Deinitializes the `TemporaryFile` instance and removes the associated temporary file from the filesystem.
     deinit {
         try? FileManager.default.removeItem(at: url)
     }
 }
 
+/// A utility class for managing a temporary folder with customizable files.
+///
+/// The `TempFolderManager` class facilitates the creation, management, 
+/// and cleanup of a temporary folder with associated files.
+///
+/// Example usage:
+/// ```swift
+/// let folderManager = TempFolderManager()
+/// folderManager.createCustomFolder()
+/// folderManager.createFiles()
+/// // Use files within the custom folder as needed
+/// // The folder and files will be automatically cleaned up upon the `TempFolderManager` instance deinitialization.
+/// ```
 class TempFolderManager {
     let temporaryDirectoryURL: URL
     let customFolderURL: URL
 
     init() {
         self.temporaryDirectoryURL = URL(fileURLWithPath: NSTemporaryDirectory())
-        self.customFolderURL = temporaryDirectoryURL.appendingPathComponent("MyCustomFolder")
+        self.customFolderURL = temporaryDirectoryURL.appendingPathComponent("TestingFolder")
     }
 
     deinit {
