c074bb33898b678c836f7ca5a5aff068774d1364
Austin Condiff <austin.condiff@gmail.com>
2024-01-08 23:12:11 -0600

    Khans updates to History view

--- CodeEdit/Features/NavigatorArea/SourceControlNavigator/History/Views/SourceControlNavigatorHistoryView.swift
+++ CodeEdit/Features/NavigatorArea/SourceControlNavigator/History/Views/SourceControlNavigatorHistoryView.swift
@@ -9,47 +9,88 @@ import SwiftUI
 import CodeEditSymbols
 
 struct SourceControlNavigatorHistoryView: View {
+    enum Status {
+        case loading
+        case ready
+        case error(error: Error)
+    }
+
     @EnvironmentObject var sourceControlManager: SourceControlManager
 
+    @State var commitHistoryStatus: Status = .loading
     @State var commitHistory: [GitCommit] = []
 
     @State var selection: GitCommit?
 
     func updateCommitHistory() async {
         do {
+            commitHistoryStatus = .loading
             let commits = try await sourceControlManager
                 .gitClient
                 .getCommitHistory(branchName: sourceControlManager.currentBranch?.name)
-            commitHistory = commits
+            await MainActor.run {
+                commitHistory = commits
+                commitHistoryStatus = .ready
+            }
         } catch {
-            commitHistory = []
+            await MainActor.run {
+                commitHistory = []
+                commitHistoryStatus = .error(error: error)
+            }
         }
     }
 
     var body: some View {
         Group {
-            if commitHistory.isEmpty {
-                CEContentUnavailableView("No History")
-            } else {
-                ZStack {
-                    List(selection: $selection) {
-                        ForEach(commitHistory) { commit in
-                            CommitListItemView(commit: commit)
-                                .tag(commit)
-                                .listRowSeparator(.hidden)
+            switch commitHistoryStatus {
+            case .loading:
+                VStack {
+                    Spacer()
+                    ProgressView {
+                        Text("Loading History")
+                    }
+                    Spacer()
+                }
+            case .ready:
+                if commitHistory.isEmpty {
+                    CEContentUnavailableView("No History")
+                } else {
+                    ZStack {
+                        List(selection: $selection) {
+                            ForEach(commitHistory) { commit in
+                                CommitListItemView(commit: commit)
+                                    .tag(commit)
+                                    .listRowSeparator(.hidden)
+                            }
+                        }
+                        .opacity(selection == nil ? 1 : 0)
+                        if selection != nil {
+                            CommitDetailsView(commit: $selection)
                         }
                     }
-                    .opacity(selection == nil ? 1 : 0)
-                    if selection != nil {
-                        CommitDetailsView(commit: $selection)
+                }
+            case .error(let error):
+                VStack {
+                    Spacer()
+                    CEContentUnavailableView(
+                        "Error Loading History",
+                        description: error.localizedDescription,
+                        systemImage: "exclamationmark.triangle"
+                    ) {
+                        Button {
+                            Task {
+                                await updateCommitHistory()
+                            }
+                        } label: {
+                            Text("Retry")
+                        }
                     }
+                    Spacer()
                 }
             }
         }
-        .onAppear {
-            Task {
-                await updateCommitHistory()
-            }
+        .task {
+            await updateCommitHistory()
         }
     }
 }
