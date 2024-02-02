8c6c23b95f268c02c774939dfcb14b8b10d41c62
Tom Ludwig <tommludwig@icloud.com>
2024-02-02 13:47:38 +0100

    Added tests

--- CodeEditTests/Features/Search/FuzzySearch/FuzzySearchTests.swift
+++ CodeEditTests/Features/Search/FuzzySearch/FuzzySearchTests.swift
@@ -6,36 +6,61 @@
 //
 
 import XCTest
+@testable import CodeEdit
 
 final class FuzzySearchTests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-
-        // In UI tests it is usually best to stop immediately when a failure occurs.
-        continueAfterFailure = false
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+    func testNormalisation() {
+        XCTAssertEqual("ü".normalise()[0].normalisedContent, "u")
+        XCTAssertEqual("ñ".normalise()[0].normalisedContent, "n")
+        XCTAssertEqual("é".normalise()[0].normalisedContent, "e")
     }
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // UI tests must launch the application that they test.
-        let app = XCUIApplication()
-        app.launch()
-
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testLaunchPerformance() throws {
-        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
-            // This measures how long it takes to launch your application.
-            measure(metrics: [XCTApplicationLaunchMetric()]) {
-                XCUIApplication().launch()
-            }
+    func testFuzzyMatchWeight() {
+        guard let url = URL(string: "path/ContentView.swift") else {
+            XCTFail("URL could not be created")
+            return
         }
+        XCTAssert(url.fuzzyMatch(query: "CV").weight > 0)
+        XCTAssert(url.fuzzyMatch(query: "conv").weight > 0)
+        XCTAssert(url.fuzzyMatch(query: "sw").weight > 0)
+        XCTAssert(url.fuzzyMatch(query: "path").weight == 0)
+    }
+
+    func testFuzzyMatchRange() {
+        guard let url = URL(string: "path/ContentView.swift") else {
+            XCTFail("URL could not be created")
+            return
+        }
+        let range = url.fuzzyMatch(query: "ConVie").matchedParts
+
+        XCTAssertEqual(url.lastPathComponent[range[0]], "Con")
+        XCTAssertEqual(url.lastPathComponent[range[1]], "Vie")
+    }
+
+    func testFuzzySearch() async {
+        let urls = [
+            URL(string: "FuzzySearchable.swift")!,
+            URL(string: "ContentView.swift")!,
+            URL(string: "FuzzyMatch.swift")!
+        ]
+        let fuzzyMatchResult = await urls.fuzzySearch(query: "mch").map {
+            $0.item
+        }
+        XCTAssertEqual(fuzzyMatchResult[0].lastPathComponent, "FuzzyMatch.swift")
+
+        let contentViewResult = await urls.fuzzySearch(query: "CV").map {
+            $0.item
+        }
+        XCTAssertEqual(contentViewResult[0].lastPathComponent, "ContentView.swift")
+
+        let fuzzySearchableResult = await urls.fuzzySearch(query: "seable").map {
+            $0.item
+        }
+        XCTAssertEqual(fuzzySearchableResult[0].lastPathComponent, "FuzzySearchable.swift")
+
+        let swiftResults = await urls.fuzzySearch(query: "swif").map {
+            $0.item
+        }
+        XCTAssertEqual(swiftResults.count, 3)
     }
 }
