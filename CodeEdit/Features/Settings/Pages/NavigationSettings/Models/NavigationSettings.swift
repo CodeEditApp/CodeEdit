8e40f5dd6c9437e75890ed0773859259c543fbcb
Austin Condiff <austin.condiff@gmail.com>
2024-03-05 02:04:18 -0600

    Added navigation style setting

---
+++ CodeEdit/Features/Settings/Pages/NavigationSettings/Models/NavigationSettings.swift
@@ -0,0 +1,8 @@
+//
+//  NavigationSettings.swift
+//  CodeEdit
+//
+//  Created by Austin Condiff on 3/4/24.
+//
+
+import Foundation
---
+++ CodeEdit/Features/Settings/Pages/NavigationSettings/NavigationSettingsView.swift
@@ -0,0 +1,18 @@
+//
+//  NavigationSettingsView.swift
+//  CodeEdit
+//
+//  Created by Austin Condiff on 3/4/24.
+//
+
+import SwiftUI
+
+struct NavigationSettingsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NavigationSettingsView()
+}
