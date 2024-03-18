1f099d959c07b723bd5ff279a57fd44f522da6d8
Phillip Song <103433299+phlpsong@users.noreply.github.com>
2024-03-19 04:21:03 +0800

    Git branch picker not load branch list and switch branch issue (#1637)
    * fix: branch picker refresh issue and switch branch issue
    
    * fix: source control history view not refresh after switch branch

--- CodeEdit/Features/CodeEditUI/Views/ToolbarBranchPicker.swift
+++ CodeEdit/Features/CodeEditUI/Views/ToolbarBranchPicker.swift
@@ -83,6 +83,7 @@ struct ToolbarBranchPicker: View {
         }
         .task {
             await self.sourceControlManager?.refreshCurrentBranch()
+            await self.sourceControlManager?.refreshBranches()
         }
     }
 
@@ -160,9 +161,6 @@ struct ToolbarBranchPicker: View {
             .padding(.top, 10)
             .padding(5)
             .frame(width: 340)
-            .task {
-                await sourceControlManager.refreshBranches()
-            }
         }
 
         func headerLabel(_ title: String) -> some View {
@@ -194,9 +192,6 @@ struct ToolbarBranchPicker: View {
                 self.title = title
             }
 
-            @Environment(\.dismiss)
-            private var dismiss
-
             var body: some View {
                 Button {
                     switchBranch()
@@ -216,9 +211,6 @@ struct ToolbarBranchPicker: View {
                 Task {
                     do {
                         try await sourceControlManager.checkoutBranch(branch: branch)
-                        await MainActor.run {
-                            dismiss()
-                        }
                     } catch {
                         await sourceControlManager.showAlertForError(title: "Failed to checkout", error: error)
                     }
