a2dfb4235e9854cbe124e76aad0e9bb35bd1d424
Abe M <abe.malla8@gmail.com>
2024-04-03 00:28:31 -0700

    Added dependency injection

---
+++ CodeEdit/Features/DependencyInjection/ServiceContainer.swift
@@ -0,0 +1,44 @@
+//
+//  ServiceContainer.swift
+//  CodeEdit
+//
+//  Created by Abe Malla on 4/3/24.
+//
+
+enum ServiceContainer {
+    private static var factories: [String: () -> Any] = [:]
+    private static var cache: [String: Any] = [:]
+
+    static func register<Service>(type: Service.Type, _ factory: @autoclosure @escaping () -> Service) {
+        factories[String(describing: type.self)] = factory
+    }
+
+    static func resolve<Service>(_ resolveType: ServiceType = .singleton, _ type: Service.Type) -> Service? {
+        let serviceName = String(describing: type.self)
+
+        switch resolveType {
+        case .singleton:
+            if let service = cache[serviceName] as? Service {
+                return service
+            } else {
+                let service = factories[serviceName]?() as? Service
+
+                if let service = service {
+                    cache[serviceName] = service
+                }
+
+                return service
+            }
+        case .newSingleton:
+            let service = factories[serviceName]?() as? Service
+
+            if let service = service {
+                cache[serviceName] = service
+            }
+
+            return service
+        case .new:
+            return factories[serviceName]?() as? Service
+        }
+    }
+}
---
+++ CodeEdit/Features/DependencyInjection/ServiceType.swift
@@ -0,0 +1,15 @@
+//
+//  ServiceType.swift
+//  CodeEdit
+//
+//  Created by Abe Malla on 4/3/24.
+//
+
+enum ServiceType {
+    /// Returns a new singleton on the first call, then returns a cached one every other time
+    case singleton
+    /// Creates a new singleton reference each time and caches it, returning the newer singleton
+    case newSingleton
+    /// Creates a new singleton
+    case new
+}
