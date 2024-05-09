4238f2c2f8700536c42f12dc03d239453b9f91a8
Paul Ebose <paulebose@gmail.com>
2024-05-09 17:20:20 +0100

    remove `WorkspaceCannotPreviewFileView` as it creates a bug that makes thumbnailing slightly unpredictable
    Will use QLPreview instead

--- CodeEdit/Features/Documents/Views/WorkspaceImageView.swift
+++ CodeEdit/Features/Documents/Views/WorkspaceImageView.swift
@@ -44,7 +44,7 @@ struct WorkspaceImageView: View {
                 .frame(width: proxy.size.width, height: proxy.size.height)
             }
         } else {
-            WorkspaceCannotPreviewFileView(imageURL)
+            Text("Cannot preview image")
         }
     }
 
