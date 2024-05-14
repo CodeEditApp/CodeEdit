5724c724d66ea2af9e2b78b0fbbb91eb78c33f4c
Shivam Parekh <sparekh1248@gmail.com>
2024-05-14 17:53:32 -0400

    New tabs after current focused tab#1704 (#1715)

--- CodeEdit/Features/Editor/Models/Editor.swift
+++ CodeEdit/Features/Editor/Models/Editor.swift
@@ -195,7 +195,13 @@ final class Editor: ObservableObject, Identifiable {
         if let index {
             tabs.insert(item, at: index)
         } else {
-            tabs.append(item)
+            guard let currentTab = selectedTab, let currentIndex = tabs.firstIndex(of: currentTab)
+            else {
+                tabs.append(item)
+                return
+            }
+            let nextIndex = tabs.index(after: currentIndex)
+            tabs.insert(item, at: nextIndex)
         }
         selectedTab = item
         if !fromHistory {
