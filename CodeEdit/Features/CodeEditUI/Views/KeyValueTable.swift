d0dd45bd92465ebbabdd3d7674900556fa472814
Abe M <abe.malla8@gmail.com>
2024-05-31 14:28:27 -0700

    Remove commented line

--- CodeEdit/Features/CodeEditUI/Views/KeyValueTable.swift
+++ CodeEdit/Features/CodeEditUI/Views/KeyValueTable.swift
@@ -75,7 +75,6 @@ private struct NewListTableItemView: View {
                 .disabled(key.isEmpty || value.isEmpty)
             }
             .padding(.horizontal, 20)
-//            .padding(.top, 2)
             .padding(.bottom, 20)
         }
         .frame(maxWidth: 480)
