bfdd9ad3156625f4b55aca358dfa5af63a12bbc4
Pedro <pedro@pepicrft.me>
2024-06-11 14:56:28 +0200

    Declare dependencies

---
+++ Tuist/Package.swift
@@ -0,0 +1,29 @@
+// swift-tools-version:5.9
+
+import PackageDescription
+
+#if TUIST
+    import ProjectDescription
+
+    let packageSettings = PackageSettings(
+        // Override integration settings
+    )
+#endif
+
+let package = Package(
+    name: "CodeEdit",
+    platforms: [.macOS(.v12)],
+    dependencies: [
+        .package(url: "https://github.com/CodeEditApp/CodeEditSymbols", exact: "0.2.2"),
+        .package(url: "https://github.com/migueldeicaza/SwiftTerm", exact: "1.2.0"),
+        .package(url: "https://github.com/sparkle-project/Sparkle", exact: "2.3.0"),
+        .package(url: "https://github.com/pointfreeco/swift-snapshot-testing", .upToNextMajor(from: "1.14.2")),
+        .package(url: "https://github.com/apple/swift-collections", .upToNextMajor(from: "1.0.0")),
+        .package(url: "https://github.com/JohnSundell/CollectionConcurrencyKit", .upToNextMajor(from: "0.2.0")),
+        .package(url: "https://github.com/groue/GRDB.swift", .upToNextMajor(from: "5.2.0")),
+        .package(url: "https://github.com/siteline/swiftui-introspect", .upToNextMajor(from: "0.2.3")),
+        .package(url: "https://github.com/CodeEditApp/CodeEditKit", exact: "0.1.1"),
+        .package(url: "https://github.com/CodeEditApp/CodeEditSourceEditor", .upToNextMajor(from: "0.7.3")),
+        .package(url: "https://github.com/Wouter01/LogStream", .upToNextMajor(from: "1.3.0"))
+    ]
+)
