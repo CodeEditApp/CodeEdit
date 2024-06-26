721f2febd13547371bc632cfdcf29dd11c7ae5cf
Tom Ludwig <tommludwig@icloud.com>
2024-06-26 12:55:01 +0200

    Improve code readability

--- CodeEdit/Features/ActivityViewer/Tasks/TaskDropDownView.swift
+++ CodeEdit/Features/ActivityViewer/Tasks/TaskDropDownView.swift
@@ -8,12 +8,11 @@
 import SwiftUI
 
 struct TasksDropDownMenuView: View {
-    @Environment(\.colorScheme)
+    @Environment(\.colorScheme) 
     private var colorScheme
 
     @ObservedObject var taskManager: TaskManager
 
-    @State private var taskStatus: [UUID: CETaskStatus] = [:]
     @State private var isTaskPopOverPresented: Bool = false
     @State private var isHoveringTasks: Bool = false
 
@@ -24,130 +23,147 @@ struct TasksDropDownMenuView: View {
         Group {
             if let selectedTask = taskManager.selectedTask {
                 if let selectedActiveTask = taskManager.activeTasks[selectedTask.id] {
-                    CurrentTaskOverView(activeTask: selectedActiveTask)
+                    TaskView(activeTask: selectedActiveTask, isCompact: true)
                 } else {
-                    HStack(spacing: 3) {
-                        Image(systemName: "gearshape")
-                            .imageScale(.medium)
-
-                        if taskManager.availableTasks.isEmpty {
-                            Text("Create Tasks")
-                                .font(.subheadline)
-                        } else {
-                            Text(taskManager.selectedTask?.name ?? "Unknown")
-                                .font(.subheadline)
-                        }
-
-                        if let taskID = taskManager.selectedTaskID {
-                            Circle()
-                                .fill(taskManager.taskStatus(taskID).color)
-                                .frame(width: 5, height: 5)
-                        } else {
-                            Circle()
-                                .fill(CETaskStatus.stopped.color)
-                                .frame(width: 5, height: 5)
-                        }
-                    }
+                    defaultTaskView(task: selectedTask)
                 }
             } else {
                 Text("Create Tasks")
                     .font(.subheadline)
             }
         }
-        .font(.caption)
         .padding(.trailing, 9)
         .padding(5)
-        .background {
-            Color(nsColor: colorScheme == .dark ? .white : .black)
-                .opacity(isHoveringTasks || isTaskPopOverPresented ? 0.05 : 0)
-                .clipShape(RoundedRectangle(cornerSize: CGSize(width: 4, height: 4)))
-            HStack {
-                Spacer()
-                if isHoveringTasks || isTaskPopOverPresented {
-                    VStack(spacing: 1) {
-                        Image(systemName: "chevron.down")
-                    }
-                    .font(.system(size: 8, weight: .bold, design: .default))
-                    .padding(.top, 0.5)
-                    .padding(.trailing, 3)
-                }
-            }
-        }
-        .onHover(perform: { hovering in
+        .background(backgroundColor)
+        .onHover { hovering in
             self.isHoveringTasks = hovering
-        })
+        }
         .popover(isPresented: $isTaskPopOverPresented) {
-            VStack(alignment: .leading, spacing: 0) {
-                if let tasks = tasksSettings?.items, !tasks.isEmpty {
-                    ForEach(tasks, id: \.name) { item in
-                        TasksPopoverView(
-                            taskManager: taskManager,
-                            taskStatus: taskStatus,
-                            item: item
-                        )
-                    }
-                    Divider()
-                        .padding(.vertical, 5)
-                }
-
-                Group {
-                    OptionMenuItemView(label: "Add Task..") {
-                        NSApp.sendAction(
-                            #selector(CodeEditWindowController.openWorkspaceSettings(_:)), to: nil, from: nil
-                        )
-                    }
-                    OptionMenuItemView(label: "Manage Tasks...") {
-                        NSApp.sendAction(
-                            #selector(CodeEditWindowController.openWorkspaceSettings(_:)), to: nil, from: nil
-                        )
-                    }
-                }
-            }
-            .padding(5)
-            .frame(width: 215)
+            taskPopoverContent
         }
         .onTapGesture {
             self.isTaskPopOverPresented.toggle()
         }
-        .onReceive(taskManager.$activeTasks, perform: { activeTasks in
-            taskStatus.removeAll()
-            for (key, value) in activeTasks {
-                taskStatus[key] = value.status
-            }
-        })
     }
-}
 
-struct CurrentTaskOverView: View {
-    @ObservedObject var activeTask: CEActiveTask
-    var body: some View {
+    private var backgroundColor: some View {
+        Color(nsColor: colorScheme == .dark ? .white : .black)
+            .opacity(isHoveringTasks || isTaskPopOverPresented ? 0.05 : 0)
+            .clipShape(RoundedRectangle(cornerSize: CGSize(width: 4, height: 4)))
+            .overlay(
+                HStack {
+                    Spacer()
+                    if isHoveringTasks || isTaskPopOverPresented {
+                        chevronIcon
+                    }
+                }
+            )
+    }
+
+    private var chevronIcon: some View {
+        VStack(spacing: 1) {
+            Image(systemName: "chevron.down")
+                .font(.system(size: 8, weight: .bold, design: .default))
+                .padding(.top, 0.5)
+                .padding(.trailing, 3)
+        }
+    }
+
+    private func defaultTaskView(task: CETask) -> some View {
         HStack(spacing: 3) {
             Image(systemName: "gearshape")
                 .imageScale(.medium)
 
+            Text(task.name)
+                .font(.subheadline)
+
+            Circle()
+                .fill(CETaskStatus.stopped.color)
+                .frame(width: 5, height: 5)
+        }
+    }
+
+    private var taskPopoverContent: some View {
+        VStack(alignment: .leading, spacing: 0) {
+            if !taskManager.availableTasks.isEmpty {
+                ForEach(taskManager.availableTasks, id: \.id) { task in
+                    TasksPopoverView(taskManager: taskManager, task: task)
+                }
+                Divider()
+                    .padding(.vertical, 5)
+            }
+
+            Group {
+                OptionMenuItemView(label: "Add Task..") {
+                    NSApp.sendAction(#selector(CodeEditWindowController.openWorkspaceSettings(_:)), to: nil, from: nil)
+                }
+                OptionMenuItemView(label: "Manage Tasks...") {
+                    NSApp.sendAction(#selector(CodeEditWindowController.openWorkspaceSettings(_:)), to: nil, from: nil)
+                }
+            }
+        }
+        .padding(5)
+        .frame(width: 215)
+    }
+}
+
+// We need to observe each active task individually because:
+// 1. Active tasks are nested inside TaskManager.
+// 2. Reference types (like objects) do not notify observers when their internal state changes.
+/// `TaskView` represents a single active task and observes its state.
+/// - Parameter activeTask: The active task to be displayed and observed.
+/// - Parameter isCompact: Determines the layout style of the view.
+///   Set to `true` for a compact display, used for the current task in the activity viewer.
+///   Set to `false`, used in the popover.
+struct TaskView: View {
+    @ObservedObject var activeTask: CEActiveTask
+    // This property allows for compact layout adjustment
+    var isCompact: Bool
+
+    var body: some View {
+        HStack(spacing: isCompact ? 3 : 8) {
+            Image(systemName: "gearshape")
+                .imageScale(.medium)
+
             Text(activeTask.task.name)
-                    .font(.subheadline)
+                .font(isCompact ? .subheadline : .body)
+
+            if !isCompact {
+                Spacer()
+            }
 
             Circle()
                 .fill(activeTask.status.color)
                 .frame(width: 5, height: 5)
         }
-        .font(.caption)
     }
 }
 
 struct TasksPopoverView: View {
-    @Environment(\.dismiss)
+    @Environment(\.dismiss) 
     private var dismiss
 
     @ObservedObject var taskManager: TaskManager
-
-    var taskStatus: [UUID: CETaskStatus] = [:]
-    var item: CETask
+    var task: CETask
 
     var body: some View {
         HStack {
-            if taskManager.selectedTaskID == item.id {
+            selectionIndicator
+            popoverContent
+        }
+        .padding(.vertical, 5)
+        .padding(.horizontal, 10)
+        .modifier(DropdownMenuItemStyleModifier())
+        .onTapGesture {
+            taskManager.selectedTaskID = task.id
+            dismiss()
+        }
+        .clipShape(RoundedRectangle(cornerRadius: 5))
+    }
+
+    private var selectionIndicator: some View {
+        Group {
+            if taskManager.selectedTaskID == task.id {
                 Image(systemName: "checkmark")
                     .imageScale(.small)
                     .frame(width: 10)
@@ -155,23 +171,28 @@ struct TasksPopoverView: View {
                 Spacer()
                     .frame(width: 18)
             }
+        }
+    }
+
+    private var popoverContent: some View {
+        Group {
+            if let activeTask = taskManager.activeTasks[task.id] {
+                TaskView(activeTask: activeTask, isCompact: false)
+            } else {
+                defaultTaskView
+            }
+        }
+    }
+
+    private var defaultTaskView: some View {
+        HStack {
             Image(systemName: "gearshape")
                 .imageScale(.medium)
-            Text(item.name)
-
+            Text(task.name)
             Spacer()
-
             Circle()
-                .fill(taskManager.taskStatus(item.id).color)
+                .fill(taskManager.taskStatus(task.id).color)
                 .frame(width: 5, height: 5)
         }
-        .padding(.vertical, 5)
-        .padding(.horizontal, 10)
-        .modifier(DropdownMenuItemStyleModifier())
-        .onTapGesture {
-            taskManager.selectedTaskID = item.id
-            dismiss()
-        }
-        .clipShape(RoundedRectangle(cornerRadius: 5))
     }
 }
