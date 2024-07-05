6d2e4e117a1d31b7d8c1e33fc976ef99f85067be
Paul Ebose <paulebose@gmail.com>
2024-07-05 12:14:31 +0100

    create `Collection+subscript_safe.swift`

--- CodeEdit/Features/Git/Client/GitClient.swift
+++ CodeEdit/Features/Git/Client/GitClient.swift
@@ -76,11 +76,3 @@ class GitClient {
         return output
     }
 }
-
-internal extension Collection {
-    /// Returns the element at the specified index if it is within bounds, otherwise nil.
-    subscript (safe index: Index) -> Element? {
-        indices.contains(index) ? self[index] : nil
-    }
-}
-
