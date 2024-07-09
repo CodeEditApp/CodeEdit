e0009d2c2dbf0b717f1215324366a7ce63b5a4f5
Paul Ebose <49006567+plbstl@users.noreply.github.com>
2024-07-09 16:45:30 +0100

    fix 'Open Quickly' fatal error (#1793)
    * fix open quickly fatal error
    
    * create `String+escapedWhiteSpaces.swift`
    
    * create `Collection+subscript_safe.swift`

--- CodeEdit/Features/Git/Client/GitClient.swift
+++ CodeEdit/Features/Git/Client/GitClient.swift
@@ -76,16 +76,3 @@ class GitClient {
         return output
     }
 }
-
-internal extension Collection {
-    /// Returns the element at the specified index if it is within bounds, otherwise nil.
-    subscript (safe index: Index) -> Element? {
-        indices.contains(index) ? self[index] : nil
-    }
-}
-
-internal extension String {
-    func escapedWhiteSpaces() -> String {
-        self.replacingOccurrences(of: " ", with: "\\ ")
-    }
-}
