be21f9d80f7f5f44fa8631eb417d982a0bb451f8
Austin Condiff <austin.condiff@gmail.com>
2024-08-14 07:23:59 -0500

    Enabled git push force and tags options (#1846)
    * Enabled git push force and tags.

--- CodeEdit/Features/SourceControl/Client/GitClient+Push.swift
+++ CodeEdit/Features/SourceControl/Client/GitClient+Push.swift
@@ -9,11 +9,23 @@ import Foundation
 
 extension GitClient {
     /// Push changes to remote
-    func pushToRemote(remote: String? = nil, branch: String? = nil, setUpstream: Bool? = false ) async throws {
+    func pushToRemote(
+        remote: String? = nil,
+        branch: String? = nil,
+        setUpstream: Bool? = false,
+        force: Bool? = false,
+        tags: Bool? = false
+    ) async throws {
         var command = "push"
         if let remote, let branch {
             if setUpstream == true {
                 command += " --set-upstream"
+            }
+            if force == true {
+                command += " --force"
+            }
+            if tags == true {
+                command += " --tags"
             }
             command += " \(remote) \(branch)"
         }
