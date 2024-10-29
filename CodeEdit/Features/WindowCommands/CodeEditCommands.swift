e58ab760ea1051deffb016985fba9965e6dd3065
Austin Condiff <austin.condiff@gmail.com>
2024-10-28 22:55:59 -0500

    Hiding Source Control menu when source control is disabled in Settings.

--- CodeEdit/Features/WindowCommands/CodeEditCommands.swift
+++ CodeEdit/Features/WindowCommands/CodeEditCommands.swift
@@ -8,13 +8,16 @@
 import SwiftUI
 
 struct CodeEditCommands: Commands {
+    @AppSettings(\.sourceControl.general.enableSourceControl)
+    private var enableSourceControl
+
     var body: some Commands {
         MainCommands()
         FileCommands()
         ViewCommands()
         FindCommands()
         NavigateCommands()
-        SourceControlCommands()
+        if enableSourceControl { SourceControlCommands() }
         ExtensionCommands()
         WindowCommands()
         HelpCommands()
