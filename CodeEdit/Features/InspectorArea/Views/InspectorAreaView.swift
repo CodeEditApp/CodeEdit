0a77abd289c6a861c8d55fe82cd11193829b55e2
Austin Condiff <austin.condiff@gmail.com>
2025-01-06 01:21:24 -0800

    Updated to latest

--- CodeEdit/Features/InspectorArea/Views/InspectorAreaView.swift
+++ CodeEdit/Features/InspectorArea/Views/InspectorAreaView.swift
@@ -13,17 +13,24 @@ struct InspectorAreaView: View {
     @ObservedObject private var extensionManager = ExtensionManager.shared
     @ObservedObject public var viewModel: InspectorAreaViewModel
 
-    @EnvironmentObject private var editorManager: EditorManager
-
-    @AppSettings(\.sourceControl.general.sourceControlIsEnabled)
-    private var sourceControlIsEnabled: Bool
-
     @AppSettings(\.general.inspectorTabBarPosition)
     var sidebarPosition: SettingsData.SidebarTabBarPosition
 
     init(viewModel: InspectorAreaViewModel) {
         self.viewModel = viewModel
-        updateTabItems()
+
+        viewModel.tabItems = [.file, .gitHistory] +
+            extensionManager
+                .extensions
+                .map { ext in
+                    ext.availableFeatures.compactMap {
+                        if case .sidebarItem(let data) = $0, data.kind == .inspector {
+                            return InspectorTab.uiExtension(endpoint: ext.endpoint, data: data)
+                        }
+                        return nil
+                    }
+                }
+                .joined()
     }
 
     var body: some View {
@@ -36,28 +43,5 @@ struct InspectorAreaView: View {
         .formStyle(.grouped)
         .accessibilityElement(children: .contain)
         .accessibilityLabel("inspector")
-        .onChange(of: sourceControlIsEnabled) { _ in
-            updateTabItems()
-        }
-    }
-
-    private func updateTabItems() {
-        viewModel.tabItems = [.file] +
-            (sourceControlIsEnabled ? [.gitHistory] : []) +
-            extensionManager
-                .extensions
-                .flatMap { ext in
-                    ext.availableFeatures.compactMap {
-                        if case .sidebarItem(let data) = $0, data.kind == .inspector {
-                            return InspectorTab.uiExtension(endpoint: ext.endpoint, data: data)
-                        }
-                        return nil
-                    }
-                }
-        if let selectedTab = selection,
-            !viewModel.tabItems.isEmpty &&
-            !viewModel.tabItems.contains(selectedTab) {
-            selection = viewModel.tabItems[0]
-        }
     }
 }
